e801cf35a8ff88a70b0fbcd1a5de44151242ba47
studio3104 <studio3104.com@gmail.com>
2019-11-27 18:20:02 +0900

    Enhance python/0121

--- python/0121/answer.py
+++ python/0121/answer.py
@@ -1,28 +1,15 @@
 from typing import List
 
-from itertools import groupby
-
 
 class Solution:
-    def __init__(self) -> None:
-        self.memo: List[int] = []
-
-    def max_profit(self, prices: List[int], max_profit: int = 0) -> int:
-        if len(prices) <= 1:
-            return max_profit
-
-        price = prices.pop(0)
-
-        if price not in self.memo:
-            self.memo.append(price)
-
-            profit = max(prices) - price
-
-            if max_profit < profit:
-                max_profit = profit
-
-        return self.max_profit(prices, max_profit)
-
     def maxProfit(self, prices: List[int]) -> int:
-        prices = [x[0] for x in groupby(prices)]
-        return self.max_profit(prices)
+        if not prices:
+            return 0
+        minimum = max(prices)
+        maxprofit = 0
+        for price in prices:
+            if price < minimum:
+                minimum = price
+            elif price - minimum > maxprofit:
+                maxprofit = price - minimum
+        return maxprofit
