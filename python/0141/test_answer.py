ee462a63e3e193d0b65c58c8112ebf386bd4f051
studio3104 <studio3104.com@gmail.com>
2019-11-28 14:08:13 +0900

    Update test code

--- python/0141/test_answer.py
+++ python/0141/test_answer.py
@@ -12,16 +12,18 @@ def solution() -> Solution:
 
 @pytest.mark.parametrize(('input_value', 'expected_result'), (
     (([3, 2, 0, -4], 1), True),
+    (([1, 2], 0), True),
+    (([1], -1), False),
 ))
 def test_is_symmetric(input_value: Tuple[List[int], int], expected_result: int, solution: Solution) -> None:
     arr, pos = input_value
     head = ListNode(arr.pop(0))
     current = head
-    pos_tree = None
+    pos_tree = head if pos == 0 else None
     for i, n in enumerate(arr, 1):
         current.next = ListNode(n)
         current = current.next
-        if i == pos:
+        if not pos_tree and i == pos:
             pos_tree = current
     current.next = pos_tree
 
