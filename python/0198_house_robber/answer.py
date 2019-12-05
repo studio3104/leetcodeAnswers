11205a2f65c91ba56565b68be4545144365ee519
studio3104 <studio3104.com@gmail.com>
2019-12-05 22:19:46 +0900

    Answer for 0198_house_robber

--- python/0198_house_robber/answer.py
+++ python/0198_house_robber/answer.py
@@ -1,6 +1,24 @@
-from typing import List
+from typing import Dict, List
 
 
 class Solution:
+    def __init__(self) -> None:
+        self.memo: Dict[int, int] = {}
+
+    def _rob(self, nums: List[int], index: int) -> int:
+        if len(nums) <= index:
+            return 0
+
+        if index in self.memo:
+            return self.memo[index]
+
+        larger = max(
+            self._rob(nums, index + 2) + nums[index],
+            self._rob(nums, index + 3) + nums[index],
+        )
+
+        self.memo[index] = larger
+        return larger
+
     def rob(self, nums: List[int]) -> int:
-        return 0
+        return max(self._rob(nums, 0), self._rob(nums, 1))
