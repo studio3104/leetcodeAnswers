f0920c7f40f7de4eae2a724e6194e7ec56200143
studio3104 <studio3104.com@gmail.com>
2019-12-05 23:34:42 +0900

    Answer for 0226_invert_binary_tree

---
+++ python/0226_invert_binary_tree/answer.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x: int) -> None:
+        self.val = x
+        self.left: Optional['TreeNode'] = None
+        self.right: Optional['TreeNode'] = None
+
+
+def invert_tree(node: TreeNode) -> TreeNode:
+    if not node:
+        return node
+
+    inverted = TreeNode(node.val)
+    inverted.right = invert_tree(node.left)  # type: ignore
+    inverted.left = invert_tree(node.right)  # type: ignore
+
+    return inverted
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> Optional[TreeNode]:
+        return invert_tree(root) if root else None
