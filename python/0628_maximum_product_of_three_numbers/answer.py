9ded54345db91e797e03dc4aee3ad1a69c2792b8
studio3104 <studio3104.com@gmail.com>
2019-12-09 22:17:01 +0900

    Add python/0628_maximum_product_of_three_numbers

---
+++ python/0628_maximum_product_of_three_numbers/answer.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        pass
---
+++ python/0628_maximum_product_of_three_numbers/test_answer.py
@@ -0,0 +1,19 @@
+from typing import List
+
+import pytest
+
+from answer import Solution
+
+
+@pytest.fixture
+def solution() -> Solution:
+    return Solution()
+
+
+@pytest.mark.parametrize(('input_value', 'expected_result'), (
+    ([1, 2, 3], 6),
+    ([1, 2, 3, 4], 24),
+    ([-1, -2, -3], -6),
+))
+def test_is_valid(input_value: List[int], expected_result: int, solution: Solution) -> None:
+    assert solution.maximumProduct(input_value) == expected_result
