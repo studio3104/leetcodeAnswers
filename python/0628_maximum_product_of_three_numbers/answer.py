162a431bfea7bccc3a2b19ea93e0918c5a0f6a48
studio3104 <studio3104.com@gmail.com>
2019-12-09 23:32:26 +0900

    Cut down on wastefulness!!!!!!!

--- python/0628_maximum_product_of_three_numbers/answer.py
+++ python/0628_maximum_product_of_three_numbers/answer.py
@@ -1,19 +1,10 @@
 from typing import List
 
-from functools import reduce
-
-
-def calc(nums: List[int]) -> int:
-    return reduce(
-        (lambda a, b: a * b),
-        nums,
-    )
-
 
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
         nums.sort()
         return max(
-            calc([nums[0], nums[1], nums[-1]]),
-            calc([nums[-1], nums[-2], nums[-3]]),
+            nums[0] * nums[1] * nums[-1],
+            nums[-1] * nums[-2] * nums[-3],
         )
