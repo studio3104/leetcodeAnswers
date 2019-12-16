c049713677794fb9d3ec829f385b3583d2fbb615
studio3104 <studio3104.com@gmail.com>
2019-12-17 04:08:21 +0900

    Refactor 5. Longest Palindromic Substring as sugyan suggested, became much better :)

--- python/p0005_Longest_Palindromic_Substring.py
+++ python/p0005_Longest_Palindromic_Substring.py
@@ -3,24 +3,26 @@ class Solution:
         if not string:
             return string
 
-        self.len_string = len(string)
         self.string = string
-        result = []
+        longest = ''
 
-        for i in range(self.len_string):
-            result.append(self.find_palindromic(i))
-            if self.len_string > i + 1 and string[i] == string[i + 1]:
-                result.append(self.find_palindromic(i, True))
+        for i in range(len(string)):
+            longest = self.find_longest_palindromic(i, longest, False)
+            if len(string) > i + 1 and string[i] == string[i + 1]:
+                longest = self.find_longest_palindromic(i, longest, True)
 
-        return sorted(result, key=len)[-1]
+        return longest
 
-    def find_palindromic(self, index: int, is_even: bool = False) -> str:
+    def find_longest_palindromic(self, index: int, longest_so_far: str, is_even: bool) -> str:
         index_b = index + 1 if is_even else index
         width = 1
 
-        while index >= width and self.len_string > index_b + width:
+        while index >= width and len(self.string) > index_b + width:
             if self.string[index - width] != self.string[index_b + width]:
                 break
             width += 1
+
+        if len(longest_so_far) > index_b - index + width * 2 - 1:
+            return longest_so_far
 
         return self.string[(index - width + 1):(index_b + width)]
