d30767c7e8571096b489f6b431c7d7da7753ea4d
studio3104 <studio3104.com@gmail.com>
2019-12-16 12:19:16 +0900

    Update LRU

--- python/p0146_LRU_Cache.py
+++ python/p0146_LRU_Cache.py
@@ -11,17 +11,16 @@ class LRUCache:
         if key in self.recent_keys:
             self.recent_keys.remove(key)
             self.recent_keys.append(key)
+
         return self._cache.get(key, -1)
 
     def put(self, key: int, value: int) -> None:
         if key in self.recent_keys:
             self.recent_keys.remove(key)
-            self.recent_keys.append(key)
-        else:
-            self.recent_keys.append(key)
 
+        self.recent_keys.append(key)
         self._cache[key] = value
 
         if len(self.recent_keys) > self.capacity:
-            to_remove = self.recent_keys.pop(0)
-            del self._cache[to_remove]
+            del self._cache[self.recent_keys[0]]
+            self.recent_keys = self.recent_keys[self.capacity * -1:]
