6729987e73256178ff6dec71bed7459b232173c0
studio3104 <studio3104.com@gmail.com>
2019-12-17 18:02:30 +0900

    Answer 49. Group Anagrams

--- python/p0049_Group_Anagrams.py
+++ python/p0049_Group_Anagrams.py
@@ -3,11 +3,16 @@ from typing import Dict, List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hash_memo: List[Dict[str, None]] = []
+        hash_memo: List[Dict[str, int]] = []
         memo: Dict[int, List[str]] = {}
 
         for s in strs:
-            d = {_s: None for _s in s}
+            d: Dict[str, int] = {}
+            for _s in s:
+                if _s in d:
+                    d[_s] += 1
+                else:
+                    d[_s] = 1
 
             if d not in hash_memo:
                 hash_memo.append(d)
