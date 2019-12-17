ee8645d7be2162808c47ffea87d21e2cd5b76d1e
studio3104 <studio3104.com@gmail.com>
2019-12-17 13:10:50 +0900

    Add 49. Group Anagrams

---
+++ python/tests/test_p0049_Group_Anagrams.py
@@ -0,0 +1,24 @@
+from typing import List
+
+import pytest
+
+from p0049_Group_Anagrams import Solution
+
+
+@pytest.fixture
+def solution() -> Solution:
+    return Solution()
+
+
+@pytest.mark.parametrize(('input_value', 'expected_result'), (
+    (
+        ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'],
+        [
+              ['ate', 'eat', 'tea'],
+              ['nat', 'tan'],
+              ['bat'],
+        ],
+    ),
+))
+def test_is_valid(input_value: List[str], expected_result: List[List[str]], solution: Solution) -> None:
+    assert solution.groupAnagrams(input_value) == expected_result
