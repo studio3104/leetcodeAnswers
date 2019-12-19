5d65fdef2545cc4e96782998daec2716fcb5e489
studio3104 <studio3104.com@gmail.com>
2019-12-19 14:37:56 +0900

    Add 1160. Find Words That Can Be Formed by Characters

---
+++ python/p1160_Find_Words_That_Can_Be_Formed_by_Characters.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        pass
---
+++ python/tests/test_p1160_Find_Words_That_Can_Be_Formed_by_Characters.py
@@ -0,0 +1,16 @@
+from typing import List
+
+import pytest
+
+from p1160_Find_Words_That_Can_Be_Formed_by_Characters import Solution
+
+
+@pytest.fixture
+def solution() -> Solution:
+    return Solution()
+
+
+@pytest.mark.parametrize(('input_value', 'expected_result'), (
+))
+def test_is_valid(input_value: List[str], expected_result: int, solution: Solution) -> None:
+    assert solution.countCharacters(input_value) == expected_result
