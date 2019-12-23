94228372f4c836445901e92b0d12833d0be6e0eb
studio3104 <studio3104.com@gmail.com>
2019-12-23 19:04:55 +0900

    Update 3, but I just cheated the solution...

--- python/p0003_Longest_Substring_Without_Repeating_Characters.py
+++ python/p0003_Longest_Substring_Without_Repeating_Characters.py
@@ -5,11 +5,12 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str, start_from: int = 0) -> int:
         in_use: Dict[str, int] = {}
         longest = 0
+        ii = 0
 
         for i in range(start_from, len(s)):
             if s[i] in in_use:
-                return max(longest, self.lengthOfLongestSubstring(s, in_use[s[i]] + 1))
-            in_use[s[i]] = i
-            longest += 1
+                ii = max(in_use[s[i]], ii)
+            longest = max(longest, i - ii + 1)
+            in_use[s[i]] = i + 1
 
         return longest
