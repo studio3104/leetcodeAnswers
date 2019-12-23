d462b9d9a547a76f46b02da40525c27285ea4319
studio3104 <studio3104.com@gmail.com>
2019-12-23 19:22:37 +0900

    Rename a variable

--- python/p0003_Longest_Substring_Without_Repeating_Characters.py
+++ python/p0003_Longest_Substring_Without_Repeating_Characters.py
@@ -5,12 +5,12 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         in_use: Dict[str, int] = {}
         longest = 0
-        ii = 0
+        index_last_appeared = 0
 
         for i in range(len(s)):
             if s[i] in in_use:
-                ii = max(in_use[s[i]], ii)
-            longest = max(longest, i - ii + 1)
+                index_last_appeared = max(in_use[s[i]], index_last_appeared)
+            longest = max(longest, i - index_last_appeared + 1)
             in_use[s[i]] = i + 1
 
         return longest
