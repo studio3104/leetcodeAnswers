f1a5427ff668e54fd1e267f27925e1f92513420e
studio3104 <studio3104.com@gmail.com>
2019-12-23 19:10:56 +0900

    Oops

--- python/p0003_Longest_Substring_Without_Repeating_Characters.py
+++ python/p0003_Longest_Substring_Without_Repeating_Characters.py
@@ -2,12 +2,12 @@ from typing import Dict
 
 
 class Solution:
-    def lengthOfLongestSubstring(self, s: str, start_from: int = 0) -> int:
+    def lengthOfLongestSubstring(self, s: str) -> int:
         in_use: Dict[str, int] = {}
         longest = 0
         ii = 0
 
-        for i in range(start_from, len(s)):
+        for i in range(len(s)):
             if s[i] in in_use:
                 ii = max(in_use[s[i]], ii)
             longest = max(longest, i - ii + 1)
