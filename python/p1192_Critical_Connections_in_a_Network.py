244d4ce97ceea393fd6e401704a86047b38dc7a8
studio3104 <studio3104.com@gmail.com>
2020-01-06 23:50:17 +0900

    Fxxkin' TLE

--- python/p1192_Critical_Connections_in_a_Network.py
+++ python/p1192_Critical_Connections_in_a_Network.py
@@ -1,26 +1,42 @@
-from typing import Dict, List, Set, Tuple
+from typing import Dict, List, Tuple
 
 import pytest
 
 
+def is_reachable_all(n: int, connections: List[List[int]]) -> bool:
+    neighbors: Dict[int, List[int]] = {i: [] for i in range(n)}
+
+    for c in connections:
+        neighbors[c[0]].append(c[1])
+        neighbors[c[1]].append(c[0])
+
+    visited = [False for _ in range(n)]
+    peers = []
+
+    def DFS(t: List[int], i: int) -> List[int]:
+        visited[i] = True
+        t.append(i)
+
+        for node in neighbors[i]:
+            if not visited[node]:
+                t = DFS(t, node)
+        return t
+
+    for i in range(n):
+        if not visited[i]:
+            t: List[int] = []
+            peers.append(DFS(t, i))
+
+    return len(peers) == 1
+
+
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-        hm: Dict[int, Set[int]] = {}
-
-        for c in connections:
-            if c[0] not in hm:
-                hm[c[0]] = set()
-            if c[1] not in hm:
-                hm[c[1]] = set()
-
-            hm[c[0]].add(c[1])
-            hm[c[1]].add(c[0])
-
         result: List[List[int]] = []
 
-        for node, neighbors in hm.items():
-            if len(neighbors) == 1:
-                result.append([node, neighbors.pop()])
+        for i in range(len(connections)):
+            if not is_reachable_all(n, connections[:i] + connections[i + 1:]):
+                result.append(connections[i])
 
         return result
 
@@ -31,7 +47,7 @@ def solution() -> Solution:
 
 
 @pytest.mark.parametrize(('input_value', 'expected_result'), (
-    ((4, [[0, 1], [1, 2], [2, 0], [1, 3]]), [[3, 1]]),
+    ((4, [[0, 1], [1, 2], [2, 0], [1, 3]]), [[1, 3]]),
     ((6, [[0, 1], [1, 2], [2, 0], [1, 3], [3, 4], [4, 5], [5, 3]]), [[1, 3]]),
 ))
 def test_is_valid(input_value: Tuple[int, List[List[int]]], expected_result: List[int], solution: Solution) -> None:
