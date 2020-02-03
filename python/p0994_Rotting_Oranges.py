258e0de70253d1d7de4646f65bf204c5bab14e26
studio3104 <studio3104.com@gmail.com>
2020-02-03 18:23:04 +0900

    Add 994. Rotting Oranges

--- python/p0994_Rotting_Oranges.py
+++ python/p0994_Rotting_Oranges.py
@@ -1,6 +1,21 @@
 from typing import List
+import pytest
 
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         pass
+
+
+@pytest.fixture
+def solution() -> Solution:
+    return Solution()
+
+
+@pytest.mark.parametrize(('input_value', 'expected_result'), (
+    ([[2, 1, 1], [1, 1, 0], [0, 1, 1]], 4),
+    ([[2, 1, 1], [0, 1, 1], [1, 0, 1]], -1),
+    ([0, 2], 0),
+))
+def test_is_valid(input_value: List[List[int]], expected_result: int, solution: Solution) -> None:
+    assert solution.orangesRotting(input_value) == expected_result
