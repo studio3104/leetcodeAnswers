e99ded7b33ccc06788a5adb884623f3a801aa41c
studio3104 <studio3104.com@gmail.com>
2020-04-01 22:13:52 +0900

    Answer for Day 1

---
+++ 30-Day-LeetCoding-Challenge/day01.py
@@ -0,0 +1,35 @@
+from typing import Dict, List
+
+import pytest
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        memo: Dict[int, int] = {}
+
+        for n in nums:
+            if n not in memo:
+                memo[n] = 0
+            memo[n] += 1
+
+        for num, count in memo.items():
+            if count == 1:
+                return num
+
+        return 0  # It won't be evaluated, Just to avoid MyPy error.
+
+
+class TestSolution:
+    @pytest.fixture
+    def solution(self) -> Solution:
+        return Solution()
+
+    @pytest.mark.parametrize('method_name', ('singleNumber', ))
+    @pytest.mark.parametrize(('input_value', 'expected_value'), (
+        ([2, 2, 1], 1),
+    ))
+    def test_single_number(
+            self, solution: Solution, method_name: str,
+            input_value: List[int], expected_value: int) -> None:
+
+        assert getattr(solution, method_name)(input_value) == expected_value
