11ec742082f078f8cedaa64b12039f4cdf47aa6e
studio3104 <studio3104.com@gmail.com>
2020-04-02 20:48:23 +0900

    Answer for Day 2

--- 30-Day-LeetCoding-Challenge/day02.py
+++ 30-Day-LeetCoding-Challenge/day02.py
@@ -1,9 +1,28 @@
+from typing import Dict
+
 import pytest
 
 
 class Solution:
+    def __init__(self) -> None:
+        self.m: Dict[int, None] = {}
+
+    def square_sum(self, n: int) -> int:
+        answer = 0
+        for num in list(str(n)):
+            answer += int(num) ** 2
+
+        return answer
+
     def isHappy(self, n: int) -> bool:
-        pass
+        if n == 1:
+            return True
+
+        if n in self.m:
+            return False
+
+        self.m[n] = None
+        return self.isHappy(self.square_sum(n))
 
 
 class TestSolution:
@@ -14,6 +33,9 @@ class TestSolution:
     @pytest.mark.parametrize('method_name', ('isHappy', ))
     @pytest.mark.parametrize(('input_value', 'expected_value'), (
         (19, True),
+        (18, False),
+        (7, True),
+        (10, True),
     ))
     def test_single_number(
             self, solution: Solution, method_name: str,
