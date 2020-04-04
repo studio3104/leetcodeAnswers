a3bf509bc14503c8d243e37976f7ead24e0dc6d0
studio3104 <studio3104.com@gmail.com>
2020-04-05 03:00:50 +0900

    Answer for Day 4

---
+++ 30-Day-LeetCoding-Challenge/day04.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        for _ in range(nums.count(0)):
+            nums.remove(0)
+            nums.append(0)
