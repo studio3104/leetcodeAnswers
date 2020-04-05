02496facbb03ca18115a5566d2bb139056fb6921
studio3104 <studio3104.com@gmail.com>
2020-04-05 23:46:07 +0900

    Add Day5 of 30-day challenge

---
+++ 30-Day-LeetCoding-Challenge/day05.py
@@ -0,0 +1,26 @@
+from typing import List
+
+import pytest
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        pass
+
+
+class TestSolution:
+    @pytest.fixture
+    def solution(self) -> Solution:
+        return Solution()
+
+    @pytest.mark.parametrize('method_name', ('maxProfit', ))
+    @pytest.mark.parametrize(('input_value', 'expected_value'), (
+        ([7, 1, 5, 3, 6, 4], 7),
+        ([1, 2, 3, 4, 5], 4),
+        ([7, 6, 4, 3, 1], 0),
+    ))
+    def test_solution(
+            self, solution: Solution, method_name: str,
+            input_value: List[int], expected_value: int) -> None:
+
+        assert getattr(solution, method_name)(input_value) == expected_value
