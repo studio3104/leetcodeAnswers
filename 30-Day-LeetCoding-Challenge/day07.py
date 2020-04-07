70d9530123ff60c1d7e746fecdee5b593af9a471
studio3104 <studio3104.com@gmail.com>
2020-04-07 22:51:30 +0900

    Answer for Day 7

--- 30-Day-LeetCoding-Challenge/day07.py
+++ 30-Day-LeetCoding-Challenge/day07.py
@@ -1,11 +1,24 @@
-from typing import List
+from typing import Dict, List
 
 import pytest
 
 
 class Solution:
     def countElements(self, arr: List[int]) -> int:
-        pass
+        result: int = 0
+        counter: Dict[int, int] = {}
+
+        for n in arr:
+            if n not in counter:
+                counter[n] = 0
+            counter[n] += 1
+
+        for n in arr:
+            np = n + 1
+            if np in counter and counter[np] > 0:
+                result += 1
+
+        return result
 
 
 class TestSolution:
@@ -19,6 +32,7 @@ class TestSolution:
         ([1, 1, 3, 3, 5, 5, 7, 7], 0),
         ([1, 3, 2, 3, 5, 0], 3),
         ([1, 1, 2, 2], 2),
+        ([1, 1, 2], 2),
     ))
     def test_solution(
             self, solution: Solution, method_name: str,
