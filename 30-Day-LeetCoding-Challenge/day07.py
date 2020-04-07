02326e33dc3f6c2d012e14412b2893d916706934
studio3104 <studio3104.com@gmail.com>
2020-04-07 23:10:18 +0900

    Add an alternative solution for Day7

--- 30-Day-LeetCoding-Challenge/day07.py
+++ 30-Day-LeetCoding-Challenge/day07.py
@@ -4,6 +4,9 @@ import pytest
 
 
 class Solution:
+    def countElements2(self, arr: List[int]) -> int:
+        return len([i for i in range(len(arr)) if arr[i] + 1 in arr])
+
     def countElements(self, arr: List[int]) -> int:
         counter: Dict[int, int] = {}
 
@@ -20,7 +23,7 @@ class TestSolution:
     def solution(self) -> Solution:
         return Solution()
 
-    @pytest.mark.parametrize('method_name', ('countElements', ))
+    @pytest.mark.parametrize('method_name', ('countElements', 'countElements2', ))
     @pytest.mark.parametrize(('input_value', 'expected_value'), (
         ([1, 2, 3], 2),
         ([1, 1, 3, 3, 5, 5, 7, 7], 0),
