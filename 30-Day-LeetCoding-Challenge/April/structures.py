4a78546e4bd040c4ef71850b39408d888974352a
studio3104 <studio3104.com@gmail.com>
2020-05-07 14:03:03 +0900

    Implement TreeNode class

--- 30-Day-LeetCoding-Challenge/April/structures.py
+++ 30-Day-LeetCoding-Challenge/April/structures.py
@@ -1,4 +1,6 @@
-from typing import Iterator, List, Optional
+from typing import Deque, Iterator, List, Optional
+
+from collections import deque
 
 
 class ListNode:
@@ -20,3 +22,53 @@ class ListNode:
         current = cls(values.pop(-1))
         current.next = list_node
         return cls.from_list(values, current)
+
+
+class TreeNode:
+    def __init__(
+            self, val: Optional[int] = 0,
+            left: Optional['TreeNode'] = None, right: Optional['TreeNode'] = None,
+    ) -> None:
+
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __iter__(self) -> Iterator[Optional[int]]:
+        q: Deque[Optional['TreeNode']] = deque()
+        q.append(self)
+
+        while len(q) > 0:
+            node = q.popleft()
+
+            if node is not None:
+                yield node.val
+                q.append(node.left)
+                q.append(node.right)
+
+    @classmethod
+    def from_list(cls, values: List[int]) -> 'TreeNode':
+        if not values:
+            return cls(None)
+
+        if values[0] is None:
+            raise ValueError('First element must NOT be `None`')
+
+        all_nodes: Deque['TreeNode'] = deque()
+        to_be_parent: List['TreeNode'] = []
+
+        for v in values:
+            node = cls(v)
+            all_nodes.append(node)
+
+            if v is not None:
+                to_be_parent.append(node)
+
+        all_nodes.popleft()
+        for tbp in to_be_parent:
+            if all_nodes:
+                tbp.left = all_nodes.popleft()
+            if all_nodes:
+                tbp.right = all_nodes.popleft()
+
+        return to_be_parent[0]
