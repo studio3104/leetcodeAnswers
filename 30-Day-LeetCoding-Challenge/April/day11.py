0b3f5de350f7ddb73d7e5de7c770527478bfb7bb
studio3104 <studio3104.com@gmail.com>
2020-05-07 15:14:44 +0900

    Answer Day11 of April

--- 30-Day-LeetCoding-Challenge/April/day11.py
+++ 30-Day-LeetCoding-Challenge/April/day11.py
@@ -7,7 +7,22 @@ import pytest
 
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        pass
+        def get_depth(node: TreeNode) -> int:
+            if node is None or node.val is None:
+                return 0
+
+            return 1 + max(get_depth(node.left), get_depth(node.right))
+
+        def get_diameter(node: TreeNode) -> int:
+            if node is None or node.val is None:
+                return 0
+
+            diameter = max(get_diameter(node.left), get_diameter(node.right))
+            depth = get_depth(node.left) + get_depth(node.right)
+
+            return max(diameter, depth)
+
+        return get_diameter(root)
 
 
 class TestSolution:
@@ -15,9 +30,17 @@ class TestSolution:
     def solution(self) -> Solution:
         return Solution()
 
-    @pytest.mark.parametrize('method_name', ('middleNode', ))
+    @pytest.mark.parametrize('method_name', ('diameterOfBinaryTree', ))
     @pytest.mark.parametrize(('input_value', 'expected_value'), (
         ([1, 2, 3, 4, 5], 3),
+        ([], 0),
+        (
+            [
+                4, -7, -3, None, None, -9, -3, 9, -7, -4, None, 6, None, -6, -6, None,
+                None, 0, 6, 5, None, 9, None, None, -1, -4, None, None, None, -2,
+            ],
+            8,
+        ),
     ))
     def test_solution(
             self, solution: Solution, method_name: str,
