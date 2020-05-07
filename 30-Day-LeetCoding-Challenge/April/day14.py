2c287649cdbc9974e9dde30b86c08b98bd59e8db
studio3104 <studio3104.com@gmail.com>
2020-05-07 19:58:29 +0900

    Answer Day14 of April

--- 30-Day-LeetCoding-Challenge/April/day14.py
+++ 30-Day-LeetCoding-Challenge/April/day14.py
@@ -1,11 +1,39 @@
 from typing import List, Tuple
 
+from collections import deque
+
 import pytest
 
 
 class Solution:
     def stringShift(self, s: str, shift: List[List[int]]) -> str:
-        pass
+        if not s:
+            return ''
+
+        amount = 0
+
+        for _s in shift:
+            sign = 1
+            if _s[0] == 0:  # Left
+                sign = -1
+
+            amount += _s[1] * sign
+
+        right_direction = True
+        if amount < 0:
+            right_direction = False  # Left
+            amount *= -1
+        amount = amount % len(s)
+
+        string = deque(s)
+
+        for _ in range(amount):
+            if right_direction:
+                string.appendleft(string.pop())
+            else:
+                string.append(string.popleft())
+
+        return ''.join(string)
 
 
 class TestSolution:
