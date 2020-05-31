63da497633e1cb322731ddc539c9badaa5da86d6
studio3104 <studio3104.com@gmail.com>
2020-05-31 19:28:33 +0900

    Give up!!1 WA!!1

--- 30-Day-LeetCoding-Challenge/May/day31.py
+++ 30-Day-LeetCoding-Challenge/May/day31.py
@@ -7,7 +7,36 @@ import pytest
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        pass
+        w1 = []
+        w2 = []
+
+        for i in range(max(len(word1), len(word2))):
+            if len(word1) > i:
+                w1.append(word1[i])
+            else:
+                w1.append('x')
+
+            if len(word2) > i:
+                w2.append(word2[i])
+            else:
+                w2.append('x')
+
+        count = 0
+        for i in range(len(w1)):
+            if w1[i] == w2[i]:
+                continue
+
+            if len(w1) > i + 1 and w1[i+1] == w2[i]:
+                del(w1[i+1])
+                w1.append('x')
+
+            if len(w2) > i + 1 and w2[i+1] == w1[i]:
+                del(w2[i+1])
+                w2.append('x')
+
+            count += 1
+
+        return count
 
 
 class TestSolution:
@@ -17,6 +46,9 @@ class TestSolution:
 
     @pytest.mark.parametrize('method_name', ('minDistance', ))
     @pytest.mark.parametrize(('input_value', 'expected_value'), (
+        (('horse', 'ros'), 3),
+        (('intention', 'execution'), 5),
+        (('plasma', 'altruism'), 6),
     ))
     def test_solution(
             self, solution: Solution, method_name: str,
