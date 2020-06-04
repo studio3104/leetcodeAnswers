6a40af3fefc342ff5c6ffbea6a8dfb9babe710b5
studio3104 <studio3104.com@gmail.com>
2020-06-04 18:01:38 +0900

    Add an alternative solution for 4th, June

--- 30-Day-LeetCoding-Challenge/June/day04.py
+++ 30-Day-LeetCoding-Challenge/June/day04.py
@@ -3,7 +3,11 @@ from typing import List
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
         s.reverse()
+
+    def reverseString2(self, s: List[str]) -> None:
+        begin, end = 0, len(s) - 1
+        while begin < end:
+            s[begin], s[end] = s[end], s[begin]
+            begin += 1
+            end -= 1
