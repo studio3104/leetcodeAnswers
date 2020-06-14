6134bc11945cee19d647a4b9c570d200ed1417d9
studio3104 <studio3104.com@gmail.com>
2020-06-14 22:44:58 +0900

    Add temporary implementations for Day14, June

--- 30-Day-LeetCoding-Challenge/June/day14.py
+++ 30-Day-LeetCoding-Challenge/June/day14.py
@@ -1,11 +1,61 @@
-from typing import List, Tuple
+from typing import Dict, List, Tuple, Union, Optional
 
 import pytest
 
+NodeType = int
+CostType = Union[int, float] 
+CostsType = Dict[NodeType, CostType]
+PathwaysType = Dict[NodeType, Dict[NodeType, CostType]]
+ProcessedType = List[int]
+
+
+def remove_excess_pathways(pathways: PathwaysType, upper_limit: int) -> PathwaysType:
+    return pathways
+
+
+def find_lowest_cost_node(costs: CostsType, processed: ProcessedType) -> Optional[NodeType]:
+    lowest_cost: CostType = float('inf')
+    lowest_cost_node: Optional[NodeType] = None
+
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+
+    return lowest_cost_node
+
 
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
-        pass
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> CostType:
+        graph: PathwaysType = {i: {} for i in range(n)}
+        costs: CostsType = {dst: float('inf')}
+        processed: ProcessedType = []
+
+        for flight in flights:
+            _src, _dst, _cost = tuple(flight)
+            graph[_src][_dst] = _cost
+
+        graph = remove_excess_pathways(graph, K)
+
+        for _d, _c in graph[src].items():
+            costs[_d] = _c
+
+        node = find_lowest_cost_node(costs, processed)
+        while node is not None:
+            cost = costs[node]
+            neighbors = graph[node]
+            for n in neighbors.keys():
+                new_cost = cost + neighbors[n]
+                if costs[n] > new_cost:
+                    costs[n] = new_cost
+            processed.append(node)
+            node = find_lowest_cost_node(costs, processed)
+
+        print(graph)
+        print(costs)
+
+        return -1 if costs[dst] == float('inf') else costs[dst]
 
 
 class TestSolution:
