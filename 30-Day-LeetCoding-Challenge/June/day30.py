2ecc20aec0ff2371c0696d33c584b7350f7c1d80
studio3104 <studio3104.com@gmail.com>
2020-06-30 20:23:01 +0900

    Refactor Day 30th, June (TLE)

--- 30-Day-LeetCoding-Challenge/June/day30.py
+++ 30-Day-LeetCoding-Challenge/June/day30.py
@@ -12,38 +12,38 @@ class Solution:
             for j, c in enumerate(b):
                 indexes[c].append((i, j))
 
-        def find_word(word: str, start_points: List[Tuple[int, int]]) -> bool:
-            def BFS(w: str, i: int, p: Tuple[int, int], m: List[Tuple[int, int]]) -> bool:
-                if len(w) <= i:
-                    return True
+        def BFS(w: str, i: int, p: Tuple[int, int], m: List[Tuple[int, int]]) -> bool:
+            if len(w) <= i:
+                return True
 
-                if p in m:
-                    return False
-                if p[0] < 0 or p[1] < 0 or p[0] >= len(board) or p[1] >= len(board[0]):
-                    return False
-                if w[i] != board[p[0]][p[1]]:
-                    return False
-
-                i += 1
-
-                return True in [
-                    BFS(word, i, (p[0] + 1, p[1]), m + [p]),
-                    BFS(word, i, (p[0] - 1, p[1]), m + [p]),
-                    BFS(word, i, (p[0], p[1] + 1), m + [p]),
-                    BFS(word, i, (p[0], p[1] - 1), m + [p]),
-                ]
-
-            if len(word) > len(board) * len(board[0]):
+            if p in m:
                 return False
-            return True in [BFS(word, 0, p, []) for p in start_points]
+            if p[0] < 0 or p[1] < 0 or p[0] >= len(board) or p[1] >= len(board[0]):
+                return False
+            if w[i] != board[p[0]][p[1]]:
+                return False
+
+            i += 1
+            return True in [
+                BFS(word, i, (p[0] + 1, p[1]), m + [p]),
+                BFS(word, i, (p[0] - 1, p[1]), m + [p]),
+                BFS(word, i, (p[0], p[1] + 1), m + [p]),
+                BFS(word, i, (p[0], p[1] - 1), m + [p]),
+            ]
 
         answer: List[str] = []
         for word in words:
+            if len(word) > len(board) * len(board[0]):
+                continue
+
             initial = word[0]
             if initial not in indexes:
                 continue
-            if find_word(word, indexes[initial]):
-                answer.append(word)
+
+            for p in indexes[initial]:
+                if BFS(word, 0, p, []):
+                    answer.append(word)
+                    break
 
         return answer
 
