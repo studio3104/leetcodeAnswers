1d49c840f6e8ca0b4428d0ced34ccdbeb1b83661
studio3104 <studio3104.com@gmail.com>
2020-07-03 16:53:54 +0900

    Add a helper to struct TreeNode objects

---
+++ java/src/main/java/com/studio3104/leetcode/structures/TreeNode.java
@@ -0,0 +1,38 @@
+package com.studio3104.leetcode.structures;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int val) { this.val = val; }
+
+    public static TreeNode createTreeNodeFromString(String data) {
+        if (data.equals("[]"))
+            return null;
+
+        data = data.substring(1, data.length() - 1);
+        String[] split = data.split(",");
+
+        int len = split.length;
+        TreeNode[] treeNodes = new TreeNode[len];
+
+        for (int i = 0; i < len; i++) {
+            if (!split[i].equals("null"))
+                treeNodes[i] = new TreeNode(Integer.parseInt(split[i]));
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (treeNodes[i] != null) {
+                int leftIndex = i * 2 + 1;
+                if (leftIndex < len)
+                    treeNodes[i].left = treeNodes[leftIndex];
+
+                int rightIndex = leftIndex + 1;
+                if (rightIndex < len)
+                    treeNodes[i].right = treeNodes[rightIndex];
+            }
+        }
+
+        return treeNodes[0];
+    }
+}
