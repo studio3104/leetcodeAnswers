3ec27ec9c8c623e3afff5563a5242385fe468aac
studio3104 <studio3104.com@gmail.com>
2020-07-06 03:42:26 +0900

    Optimize import statements

--- java/src/test/java/com/studio3104/leetcode/explore/collection/easy/array/ContainsDuplicate/SolutionTest.java
+++ java/src/test/java/com/studio3104/leetcode/explore/collection/easy/array/ContainsDuplicate/SolutionTest.java
@@ -1,13 +1,12 @@
 package com.studio3104.leetcode.explore.collection.easy.array.ContainsDuplicate;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
     Solution solution = new Solution();
