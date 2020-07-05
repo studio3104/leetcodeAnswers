80f04ebfd04e667589e88637a5aefe9fd6d2fd08
studio3104 <studio3104.com@gmail.com>
2020-07-06 04:46:38 +0900

    Refactor "Plus One" of the Easy Collection

--- java/src/main/java/com/studio3104/leetcode/explore/collection/easy/array/PlusOne/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/collection/easy/array/PlusOne/Solution.java
@@ -3,22 +3,15 @@ package com.studio3104.leetcode.explore.collection.easy.array.PlusOne;
 class Solution {
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
-            int sum = digits[i] + 1;
-
-            if (sum == 10)
-                digits[i] = 0;
-            else {
-                digits[i] = sum;
-                break;
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
             }
+            digits[i] = 0;
         }
 
-        if (digits[0] == 0) {
-            int[] result = new int[digits.length + 1];
-            System.arraycopy(new int[]{1}, 0, result, 0, 1);
-            System.arraycopy(digits, 0, result, 1, digits.length);
-            return result;
-        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
         return digits;
     }
 }
