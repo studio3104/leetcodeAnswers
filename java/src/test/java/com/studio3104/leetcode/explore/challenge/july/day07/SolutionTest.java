8f855e22f9863bb60665462afefa5e0aee87bd36
studio3104 <studio3104.com@gmail.com>
2020-07-07 18:52:17 +0900

    Answer July 7th

---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/july/day07/SolutionTest.java
@@ -0,0 +1,39 @@
+package com.studio3104.leetcode.explore.challenge.july.day07;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of(
+                        new int[][]{
+                                new int[]{0, 1, 0, 0},
+                                new int[]{1, 1, 1, 0},
+                                new int[]{0, 1, 0, 0},
+                                new int[]{1, 1, 0, 0},
+                        }, 16
+                ),
+                Arguments.of(
+                        new int[][]{
+                                new int[]{1,1},
+                                new int[]{1,1},
+                        }, 8
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void islandPerimeter(int[][] grid, int expected) {
+        int result = solution.islandPerimeter(grid);
+        assertEquals(expected, result);
+    }
+}
