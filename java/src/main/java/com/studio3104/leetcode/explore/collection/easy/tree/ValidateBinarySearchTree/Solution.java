54dda6c09b4fc3b217e191ec172c73780d9c7c6c
studio3104 <studio3104.com@gmail.com>
2020-07-10 09:16:27 +0900

    Update "Validate Binary Search Tree" of the Easy Collection

--- java/src/main/java/com/studio3104/leetcode/explore/collection/easy/tree/ValidateBinarySearchTree/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/collection/easy/tree/ValidateBinarySearchTree/Solution.java
@@ -3,15 +3,15 @@ package com.studio3104.leetcode.explore.collection.easy.tree.ValidateBinarySearc
 import com.studio3104.leetcode.structure.TreeNode;
 
 public class Solution {
-    boolean checkIsValidBST(TreeNode tree, long max, long min) {
+    boolean checkIsValidBST(TreeNode tree, Integer max, Integer min) {
         if (tree == null) return true;
 
-        if (tree.val >= min) return false;
-        if (tree.val <= max) return false;
+        if (min != null && tree.val >= min) return false;
+        if (max != null && tree.val <= max) return false;
         return checkIsValidBST(tree.left, max, tree.val) && checkIsValidBST(tree.right, tree.val, min);
     }
 
     public boolean isValidBST(TreeNode root) {
-        return checkIsValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return checkIsValidBST(root, null, null);
     }
 }
