f6470dc29d610044d1c63d0a3536d3c85dff0aa9
studio3104 <studio3104.com@gmail.com>
2020-07-11 01:51:38 +0900

    Refactor July 10th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/july/day10/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/july/day10/Solution.java
@@ -16,55 +16,25 @@ class Solution {
         Node current = head;
         Deque<Node> stack = new ArrayDeque<>();
 
-        while (current != null || !stack.isEmpty()) {
+        while (current != null) {
+            if (current.child == null && current.next == null && stack.isEmpty()) break;
+
             if (current.child == null) {
                 if (current.next == null) {
-                    if (!stack.isEmpty()) {
-                        current.next = stack.pollFirst();
-                        current.next.prev = current;
-                    } else {
-                        break;
-                    }
+                    current.next = stack.pollFirst();
+                    current.next.prev = current;
                 } else {
                     current = current.next;
                 }
-                continue;
+            } else {
+                if (current.next != null) stack.addFirst(current.next);
+                current.child.prev = current;
+                current.next = current.child;
+                current = current.child;
+                current.prev.child = null;
             }
-
-            if (current.next != null) stack.addFirst(current.next);
-            current.child.prev = current;
-            current.next = current.child;
-            current = current.child;
-        }
-
-        Node c = head;
-        while (c != null) {
-            c.child = null;
-            c = c.next;
         }
 
         return head;
-    }
-
-    public static void main(String[] args) {
-        Node n1 = new Node(1);
-        n1.next = new Node(2);
-        n1.child = new Node(3);
-
-        Node n2 = new Node(1);
-        n2.next = new Node(3);
-        n2.next.next = new Node(2);
-
-        Solution solution = new Solution();
-        solution.flatten(n1);
-
-        System.out.println("n1.val = " + n1.val);
-        System.out.println("n2.val = " + n2.val);
-        System.out.println();
-        System.out.println("n1.next = " + n1.next.val);
-        System.out.println("n2.next = " + n2.next.val);
-        System.out.println();
-        System.out.println("n1.next.next = " + n1.next.next.val);
-        System.out.println("n2.next.next = " + n2.next.next.val);
     }
 }
