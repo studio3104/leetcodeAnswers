f4b3852e83232695b8e2d79016e86dfdc7dd55ba
studio3104 <studio3104.com@gmail.com>
2020-07-12 17:20:33 +0900

    Add an alternative for July 12th

--- java/src/test/java/com/studio3104/leetcode/explore/challenge/july/day12/SolutionTest.java
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/july/day12/SolutionTest.java
@@ -2,18 +2,34 @@ package com.studio3104.leetcode.explore.challenge.july.day12;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 class SolutionTest {
     Solution solution = new Solution();
 
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of("00000010100101000001111010011100", "00111001011110000010100101000000"),
+                Arguments.of("11111111111111111111111111111101", "10111111111111111111111111111111")
+        );
+    }
+
     @ParameterizedTest
-    @CsvSource({
-            "00000010100101000001111010011100, 00111001011110000010100101000000",
-            "11111111111111111111111111111101, 10111111111111111111111111111111",
-    })
+    @MethodSource("argumentsProvider")
     void reverseBits(String nBinary, String expectedBinary) {
         int result = solution.reverseBits((int) Long.parseLong(nBinary, 2));
+        Assertions.assertEquals((int) Long.parseLong(expectedBinary, 2), result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void reverseBits1(String nBinary, String expectedBinary) {
+        int result = solution.reverseBits1((int) Long.parseLong(nBinary, 2));
         Assertions.assertEquals((int) Long.parseLong(expectedBinary, 2), result);
     }
 }
