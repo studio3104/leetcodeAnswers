28a55e057b6bcb5ada6fadd751dfdeff1d230087
studio3104 <studio3104.com@gmail.com>
2020-07-12 13:15:48 +0900

    Correct

--- java/src/main/java/com/studio3104/leetcode/contest/weekly197/NumberOfGoodPairs/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/contest/weekly197/NumberOfGoodPairs/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
     public int numIdenticalPairs(int[] nums) {
         Map<Integer, Integer> memo = new HashMap<>();
         int answer = 0;
-        
+
         for (int num : nums) {
             if (memo.containsKey(num)) answer += memo.get(num);
             memo.put(num, memo.getOrDefault(num, 0) + 1);
--- java/src/main/java/com/studio3104/leetcode/contest/weekly197/NumberOfSubstringsWithOnly1s/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/contest/weekly197/NumberOfSubstringsWithOnly1s/Solution.java
@@ -2,13 +2,13 @@ package com.studio3104.leetcode.contest.weekly197.NumberOfSubstringsWithOnly1s;
 
 public class Solution {
     public int numSub(String s) {
-        final int MOD = 10^9 + 7;
+        final int MOD = (int) Math.pow(10, 9) + 7;
         int ones = 0;
         int answer = 0;
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '1') {
-                answer = answer + ++ones;
+                answer = (answer + ++ones) % MOD;
             } else {
                 ones = 0;
             }
