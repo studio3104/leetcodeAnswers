a45ecce344a96d5c96bb1d3f89665dcb830cd1db
studio3104 <studio3104.com@gmail.com>
2020-07-12 17:29:07 +0900

    Optimize import statements

--- java/src/test/java/com/studio3104/leetcode/explore/challenge/july/day12/SolutionTest.java
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/july/day12/SolutionTest.java
@@ -3,10 +3,8 @@ package com.studio3104.leetcode.explore.challenge.july.day12;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 class SolutionTest {
