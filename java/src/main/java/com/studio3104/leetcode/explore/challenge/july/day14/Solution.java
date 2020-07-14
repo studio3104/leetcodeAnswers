09c6f92e296e96de86579540b6c910cacf8766d9
studio3104 <studio3104.com@gmail.com>
2020-07-14 17:35:14 +0900

    Update July 14th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/july/day14/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/july/day14/Solution.java
@@ -5,11 +5,10 @@ public class Solution {
     final double MD = 360.0 / 60;
 
     public double angleClock(int hour, int minutes) {
-        hour = hour == 12 ? 0 : hour;
         double h = HD * hour + HD * minutes / 60;
         double m = MD * minutes;
 
-        double answer = Math.max(h, m) - Math.min(h, m);
-        return answer <= 180 ? answer : 360.0 - answer;
+        double answer = Math.abs(h - m);
+        return Math.min(answer, 360.0 - answer);
     }
 }
