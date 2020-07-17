bf14f7e9b501d36874450c4f95128ae4825e36af
studio3104 <studio3104.com@gmail.com>
2020-07-17 16:51:48 +0900

    Answer July 17th (CHOTTO ATAMA WARUSOU NANODE ATODE NAOSU)

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/july/day17/Solution.java
@@ -0,0 +1,29 @@
+package com.studio3104.leetcode.explore.challenge.july.day17;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int n: nums) counter.put(n, counter.getOrDefault(n, 0) + 1);
+        counter = counter.entrySet()
+                .stream()
+                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (key, value) -> key, LinkedHashMap::new));
+
+        int[] elements = new int[k];
+        int index = 0;
+        for (int n: counter.keySet()) {
+            if (k <= index) break;
+            elements[index++] = n;
+        }
+
+        return elements;
+    }
+}
