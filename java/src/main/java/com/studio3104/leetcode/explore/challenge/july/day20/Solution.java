6d7f2f3f35782878eb6abb0c09ed4c553b98df03
studio3104 <studio3104.com@gmail.com>
2020-07-20 17:03:12 +0900

    Answer July 20th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/july/day20/Solution.java
@@ -0,0 +1,21 @@
+package com.studio3104.leetcode.explore.challenge.july.day20;
+
+import com.studio3104.leetcode.structure.ListNode;
+
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) head = head.next;
+        if (head == null) return null;
+
+        ListNode current = head;
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/july/day20/SolutionTest.java
@@ -0,0 +1,34 @@
+package com.studio3104.leetcode.explore.challenge.july.day20;
+
+import com.studio3104.leetcode.structure.ListNode;
+import com.studio3104.leetcode.structure.ListNodeConverter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of(
+                        new int[]{1, 2, 6, 3, 4, 5, 6}, 6,
+                        new int[]{1, 2, 3, 4, 5}
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void removeElements(
+            @ConvertWith(ListNodeConverter.class) ListNode head, int val,
+            @ConvertWith(ListNodeConverter.class) ListNode expected
+    ) {
+        ListNode result = solution.removeElements(head, val);
+        Assertions.assertEquals(expected, result);
+    }
+}
