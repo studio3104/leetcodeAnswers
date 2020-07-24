2dd2e606086036ceaa9d354695768217c2ba7995
studio3104 <studio3104.com@gmail.com>
2020-07-24 21:04:41 +0900

    Answer July 24th

---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/july/day24/SolutionTest.java
@@ -0,0 +1,29 @@
+package com.studio3104.leetcode.explore.challenge.july.day24;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of(
+                        new int[][]{{1, 2}, {3}, {3}, {}},
+                        List.of(List.of(0, 1, 3), List.of(0, 2, 3))
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void allPathsSourceTarget(int[][] graph, List<List<Integer>> expected) {
+        List<List<Integer>> result = solution.allPathsSourceTarget(graph);
+        Assertions.assertEquals(expected, result);
+    }
+}
