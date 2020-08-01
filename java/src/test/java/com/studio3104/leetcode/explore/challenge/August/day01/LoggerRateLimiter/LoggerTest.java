293a67b48cc8ed6a05bd948e064746c3be71724f
studio3104 <studio3104.com@gmail.com>
2020-08-01 22:56:45 +0900

    Answer August 1st (Logger Rate Limiter)

---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/August/day01/LoggerRateLimiter/LoggerTest.java
@@ -0,0 +1,18 @@
+package com.studio3104.leetcode.explore.challenge.August.day01.LoggerRateLimiter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LoggerTest {
+    Logger logger = new Logger();
+
+    @Test
+    void shouldPrintMessage() {
+        Assertions.assertTrue(logger.shouldPrintMessage(1, "foo"));
+        Assertions.assertTrue(logger.shouldPrintMessage(2, "bar"));
+        Assertions.assertFalse(logger.shouldPrintMessage(3, "foo"));
+        Assertions.assertFalse(logger.shouldPrintMessage(8, "bar"));
+        Assertions.assertFalse(logger.shouldPrintMessage(10, "foo"));
+        Assertions.assertTrue(logger.shouldPrintMessage(11, "foo"));
+    }
+}
