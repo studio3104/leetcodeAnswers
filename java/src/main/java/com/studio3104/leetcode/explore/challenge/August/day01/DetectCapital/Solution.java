727941b7cfb929b98ebe91541cc58c8b1fd1d417
studio3104 <studio3104.com@gmail.com>
2020-08-01 22:28:25 +0900

    Answer August 1st (Detect Capital)

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/August/day01/DetectCapital/Solution.java
@@ -0,0 +1,18 @@
+package com.studio3104.leetcode.explore.challenge.August.day01.DetectCapital;
+
+public class Solution {
+    public boolean detectCapitalUse(String word) {
+        if (word.length() <= 1) return true;
+
+        boolean isFirstUpper = Character.isUpperCase(word.charAt(0));
+        boolean isSecondUpper = Character.isUpperCase(word.charAt(1));
+        if (!isFirstUpper && isSecondUpper) return false;
+
+        for (int i = 2; i < word.length(); ++i) {
+            if (isFirstUpper && isSecondUpper && Character.isLowerCase(word.charAt(i))) return false;
+            if (!isSecondUpper && Character.isUpperCase(word.charAt(i))) return false;
+        }
+
+        return true;
+    }
+}
