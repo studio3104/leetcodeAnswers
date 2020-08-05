17a34d24f7515463705d8b8cc605fbcb8043e9b3
studio3104 <studio3104.com@gmail.com>
2020-08-05 23:45:24 +0900

    Answer August 5th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/August/day05/WordDictionary.java
@@ -0,0 +1,48 @@
+package com.studio3104.leetcode.explore.challenge.August.day05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TrieNode {
+    Map<Character, TrieNode> children = new HashMap<>();
+    boolean isWord = false;
+
+    void insert(String word) {
+        TrieNode current = this;
+        for (char c: word.toCharArray()) {
+            current.children.putIfAbsent(c, new TrieNode());
+            current = current.children.get(c);
+        }
+        current.isWord = true;
+    }
+}
+
+class WordDictionary {
+    TrieNode trie = new TrieNode();
+
+    /** Initialize your data structure here. */
+    public WordDictionary() {}
+
+    /** Adds a word into the data structure. */
+    public void addWord(String word) { trie.insert(word); }
+
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) { return search(word, trie); }
+
+    private boolean search(String word, TrieNode node) {
+        for (int i = 0; i < word.length(); ++i) {
+            char c = word.charAt(i);
+            if (c == '.') {
+                for (char x : node.children.keySet()) {
+                    if (search(word.substring(i + 1), node.children.get(x))) return true;
+                }
+                return false;
+            }
+
+            if (!node.children.containsKey(c)) return false;
+            node = node.children.get(c);
+        }
+
+        return node.isWord;
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/August/day05/WordDictionaryTest.java
@@ -0,0 +1,19 @@
+package com.studio3104.leetcode.explore.challenge.August.day05;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class WordDictionaryTest {
+    WordDictionary wordDictionary = new WordDictionary();
+
+    @Test
+    void testWordDictionary() {
+        wordDictionary.addWord("bad");
+        wordDictionary.addWord("dad");
+        wordDictionary.addWord("mad");
+        Assertions.assertFalse(wordDictionary.search("pad"));
+        Assertions.assertTrue(wordDictionary.search("bad"));
+        Assertions.assertTrue(wordDictionary.search(".ad"));
+        Assertions.assertTrue(wordDictionary.search("b.."));
+    }
+}
