5877d42ae565eb4b66e697b40c1ef2315360c916
studio3104 <studio3104.com@gmail.com>
2020-09-03 02:58:23 +0900

    Answer September 2nd (Cheated, gonna retry...)

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/September/day02/Solution.java
@@ -0,0 +1,24 @@
+package com.studio3104.leetcode.explore.challenge.September.day02;
+
+import java.util.TreeSet;
+
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+            // Find the successor of current element
+            Integer s = set.ceiling(nums[i]);
+            if (s != null && s <= nums[i] + t) return true;
+
+            // Find the predecessor of current element
+            Integer g = set.floor(nums[i]);
+            if (g != null && nums[i] <= g + t) return true;
+
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
