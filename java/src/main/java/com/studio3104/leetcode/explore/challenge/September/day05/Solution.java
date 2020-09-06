b3cd58b66f39944aa346089133c46d67e231ac2f
studio3104 <studio3104.com@gmail.com>
2020-09-06 14:55:31 +0900

    Update September 5th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/September/day05/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/September/day05/Solution.java
@@ -7,18 +7,17 @@ import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
-    LinkedList<Integer> convertBSTToList(TreeNode tree) {
-        if (tree == null) return new LinkedList<>();
-        LinkedList<Integer> lower = convertBSTToList(tree.left);
-        LinkedList<Integer> greater = convertBSTToList(tree.right);
-        lower.add(tree.val);
-        lower.addAll(greater);
-        return lower;
+    LinkedList<Integer> convertBSTToList(TreeNode tree, LinkedList<Integer> linkedList) {
+        if (tree == null) return linkedList;
+        convertBSTToList(tree.left, linkedList);
+        linkedList.add(tree.val);
+        convertBSTToList(tree.right, linkedList);
+        return linkedList;
     }
 
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        LinkedList<Integer> l1 = convertBSTToList(root1);
-        LinkedList<Integer> l2 = convertBSTToList(root2);
+        LinkedList<Integer> l1 = convertBSTToList(root1, new LinkedList<>());
+        LinkedList<Integer> l2 = convertBSTToList(root2, new LinkedList<>());
         List<Integer> allElements = new ArrayList<>();
 
         while (!l1.isEmpty() || !l2.isEmpty()) {
