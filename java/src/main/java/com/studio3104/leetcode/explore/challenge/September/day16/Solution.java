a472f17e64850b5792ef40f0d097f2493b9518dc
studio3104 <studio3104.com@gmail.com>
2020-09-16 22:20:52 +0900

    Answer September 16th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/September/day16/Solution.java
@@ -0,0 +1,62 @@
+package com.studio3104.leetcode.explore.challenge.September.day16;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TrieNode {
+    Map<Character, TrieNode> children = new HashMap<>();
+
+    void insert(String word) {
+        TrieNode current = this;
+        for (char c: word.toCharArray()) {
+            current.children.putIfAbsent(c, new TrieNode());
+            current = current.children.get(c);
+        }
+    }
+}
+
+public class Solution {
+    private int findMaximumXOR(TrieNode trie, String binary, int depth, StringBuilder sb) {
+        if (depth >= binary.length()) return Integer.parseInt(sb.toString(), 2);
+
+        char bit = binary.charAt(depth);
+        char opposite = bit == '1' ? '0' : '1';
+        TrieNode next;
+
+        if (trie.children.containsKey(opposite)) {
+            sb.append("1");
+            next = trie.children.get(opposite);
+        } else {
+            sb.append("0");
+            next = trie.children.get(bit);
+        }
+
+        return findMaximumXOR(next, binary, depth + 1, sb);
+    }
+
+    public int findMaximumXOR(int[] nums) {
+        int maxNum = Integer.MIN_VALUE;
+        for (int num: nums) maxNum = Math.max(maxNum, num);
+        int depth = Integer.toBinaryString(maxNum).length();
+
+        TrieNode trie = new TrieNode();
+        String[] paddedBinaries = new String[nums.length];
+        String paddingFormat = "%" + depth + "s";
+
+        for (int i = 0; i < nums.length; ++i) {
+            String binary = Integer.toBinaryString(nums[i]);
+            String paddedBinary = String.format(paddingFormat, binary).replace(" ", "0");
+            paddedBinaries[i] = paddedBinary;
+            trie.insert(paddedBinary);
+        }
+
+        int maxXOR = Integer.MIN_VALUE;
+
+        for (String paddedBinary: paddedBinaries) {
+            int current = findMaximumXOR(trie, paddedBinary, 0, new StringBuilder());
+            maxXOR = Math.max(maxXOR, current);
+        }
+
+        return maxXOR;
+    }
+}
