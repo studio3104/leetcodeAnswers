77d6b116223d2d1cf13f154d280476168851338a
studio3104 <studio3104.com@gmail.com>
2020-09-19 19:41:27 +0900

    Answer September 19th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/September/day19/Solution.java
@@ -0,0 +1,23 @@
+package com.studio3104.leetcode.explore.challenge.September.day19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        int lowLog10 = (int) Math.log10(low);
+        int highLog10 = (int) Math.log10(high);
+
+        List<Integer> answer = new ArrayList<>();
+
+        for (int digits = lowLog10; digits <= highLog10; ++digits) {
+            for (int i = 1; i <= 9 - digits; ++i) {
+                int d = digits, c = 0, first = i;
+                while (d >= 0) c += first++ * Math.pow(10, d--);
+                if (c >= low && c <= high) answer.add(c);
+            }
+        }
+
+        return answer;
+    }
+}
