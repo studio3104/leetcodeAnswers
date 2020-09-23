958c0961931eec618c40a972141aedfb08465ddf
studio3104 <studio3104.com@gmail.com>
2020-09-23 12:43:22 +0900

    Answer September 22nd

---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/September/day22/SolutionTest.java
@@ -0,0 +1,27 @@
+package com.studio3104.leetcode.explore.challenge.September.day22;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{3, 2, 3}, List.of(3)),
+                Arguments.of(new int[]{1, 1, 1, 3, 3, 2, 2, 2}, List.of(1, 2))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void majorityElement(int[] nums, List<Integer> expected) {
+        List<Integer> result = solution.majorityElement(nums);
+        Assertions.assertEquals(expected, result);
+    }
+}
