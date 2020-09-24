2ae0731802dfd368310e3dec55fabd2931eedeb8
studio3104 <studio3104.com@gmail.com>
2020-09-24 17:03:29 +0900

    Change significantly September 24th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/September/day24/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/September/day24/Solution.java
@@ -1,29 +1,15 @@
 package com.studio3104.leetcode.explore.challenge.September.day24;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
     public char findTheDifference(String s, String t) {
-        Map<Character, Integer> sMap = new HashMap<>();
-        Map<Character, Integer> tMap = new HashMap<>();
+        int diff = 0;
 
         for (int i = 0; i < s.length(); ++i) {
-            char sChar = s.charAt(i), tChar = t.charAt(i);
-            sMap.put(sChar, sMap.getOrDefault(sChar, 0) + 1);
-            tMap.put(tChar, tMap.getOrDefault(tChar, 0) + 1);
+            diff -= s.charAt(i);
+            diff += t.charAt(i);
         }
-        char tChar = t.charAt(t.length() - 1);
-        tMap.put(tChar, tMap.getOrDefault(tChar, 0) + 1);
+        diff += t.charAt(t.length() - 1);
 
-        char difference = ' ';
-        for (char c: tMap.keySet()) {
-            if (!sMap.getOrDefault(c, -1).equals(tMap.get(c))) {
-                difference = c;
-                break;
-            }
-        }
-
-        return difference;
+        return (char) diff;
     }
 }
