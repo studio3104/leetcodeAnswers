502502db7f6f19e40a5db0aa20bc3aa0d5574749
studio3104 <studio3104.com@gmail.com>
2020-09-26 00:06:49 +0900

    Update September 25th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/September/day25/Solution.java
@@ -0,0 +1,60 @@
+package com.studio3104.leetcode.explore.challenge.September.day25;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    List<String> sort(List<String> nums) {
+        int len = nums.size();
+        if (len <= 1) return nums;
+
+        int pIndex = len / 2;
+        String pivot = nums.get(pIndex);
+        List<String> larger = new ArrayList<>();
+        List<String> smaller = new ArrayList<>();
+
+        for (int i = 0; i < len; ++i) {
+            if (i == pIndex) continue;
+
+            String num = nums.get(i);
+            char pC0 = pivot.charAt(0), nC0 = num.charAt(0);
+            int pLen = pivot.length(), nLen = num.length();
+            boolean added = false;
+
+            for (int j = 0; j < Math.max(pLen, nLen); ++j) {
+                char pC = j >= pLen ? pC0 : pivot.charAt(j);
+                char nC = j >= nLen ? nC0 : num.charAt(j);
+
+                if (pC == nC) continue;
+                if (pC > nC) smaller.add(num);
+                else larger.add(num);
+
+                added = true;
+                break;
+            }
+
+            if (added) continue;
+
+            if (pivot.charAt(pLen - 1) > num.charAt(nLen - 1)) smaller.add(num);
+            else larger.add(num);
+        }
+
+        larger = sort(larger);
+        smaller = sort(smaller);
+
+        larger.add(pivot); larger.addAll(smaller);
+        return larger;
+    }
+
+    public String largestNumber(int[] nums) {
+        List<String> stringNums = new ArrayList<>();
+        for (int n: nums) stringNums.add(String.valueOf(n));
+        List<String> sortedStringNums = sort(stringNums);
+
+        if (sortedStringNums.get(0).equals("0")) return "0";
+
+        StringBuilder result = new StringBuilder();
+        for (String s: sortedStringNums) result.append(s);
+        return result.toString();
+    }
+}
