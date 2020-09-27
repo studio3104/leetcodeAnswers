d99da1ba075c51539dac3e93e8c92e2c554de1d6
studio3104 <studio3104.com@gmail.com>
2020-09-27 22:38:08 +0900

    Answer September 27th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/September/day27/Solution.java
@@ -0,0 +1,64 @@
+package com.studio3104.leetcode.explore.challenge.September.day27;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+    Map<String, Map<String, Double>> graph;
+
+    private double backtrack(String currentNode, String targetNode, double accProduct, Set<String> visited) {
+        visited.add(currentNode);
+        double result = -1.0;
+
+        Map<String, Double> neighbors = graph.get(currentNode);
+        if (neighbors.containsKey(targetNode)) {
+            result = accProduct * neighbors.get(targetNode);
+        } else {
+            for (Map.Entry<String, Double> pair : neighbors.entrySet()) {
+                String nextNode = pair.getKey();
+                if (visited.contains(nextNode)) continue;
+                result = backtrack(nextNode, targetNode, accProduct * pair.getValue(), visited);
+                if (result != -1.0) break;
+            }
+        }
+
+        visited.remove(currentNode);
+        return result;
+    }
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        graph = new HashMap<>();
+
+        for (int i = 0; i < values.length; ++i) {
+            List<String> equation = equations.get(i);
+            String dividend = equation.get(0);
+            String divisor = equation.get(1);
+            double quotient = values[i];
+
+            graph.putIfAbsent(dividend, new HashMap<>());
+            graph.putIfAbsent(divisor, new HashMap<>());
+            graph.get(dividend).put(divisor, quotient);
+            graph.get(divisor).put(dividend, 1 / quotient);
+        }
+
+        double[] result = new double[queries.size()];
+
+        for (int i = 0; i < queries.size(); ++i) {
+            List<String> query = queries.get(i);
+            String dividend = query.get(0);
+            String divisor = query.get(1);
+
+            if (!graph.containsKey(dividend) || !graph.containsKey(divisor)) result[i] = -1.0;
+            else if (dividend.equals(divisor)) result[i] = 1.0;
+            else {
+                HashSet<String> visited = new HashSet<>();
+                result[i] = backtrack(dividend, divisor, 1, visited);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/September/day27/SolutionTest.java
@@ -0,0 +1,43 @@
+package com.studio3104.leetcode.explore.challenge.September.day27;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(List.of("a", "b"), List.of("b", "c")),
+                        new double[]{2.0, 3.0},
+                        List.of(List.of("a", "c"), List.of("b", "a"), List.of("a", "e"), List.of("a", "a"), List.of("x", "x")),
+                        new double[]{6.00000, 0.50000, -1.00000, 1.00000, -1.00000}
+                ),
+                Arguments.of(
+                        List.of(List.of("a", "b"), List.of("b", "c"), List.of("bc", "cd")),
+                        new double[]{1.5, 2.5, 5.0},
+                        List.of(List.of("a", "c"), List.of("c", "b"), List.of("bc", "cd"), List.of("cd", "bc")),
+                        new double[]{3.75000, 0.40000, 5.00000, 0.20000}
+                ),
+                Arguments.of(
+                        List.of(List.of("a", "b")),
+                        new double[]{0.5},
+                        List.of(List.of("a", "b"), List.of("b", "a"), List.of("a", "c"), List.of("x", "y")),
+                        new double[]{0.50000, 2.00000, -1.00000, -1.00000}
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries, double[] expected) {
+        double[] result = solution.calcEquation(equations, values, queries);
+        Assertions.assertArrayEquals(expected, result);
+    }
+}
