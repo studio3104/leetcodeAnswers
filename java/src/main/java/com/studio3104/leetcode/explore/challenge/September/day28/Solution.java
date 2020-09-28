77e0b0768ef2a5a17db7a8a9d3e32e0452f235b6
studio3104 <studio3104.com@gmail.com>
2020-09-29 00:38:05 +0900

    Answer September 28th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/September/day28/Solution.java
@@ -0,0 +1,33 @@
+package com.studio3104.leetcode.explore.challenge.September.day28;
+
+public class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int topSubarray = 0;
+        int product = 1;
+        int numSubarray = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            int num = nums[i];
+
+            if (num >= k) {
+                product = 1;
+                topSubarray = i + 1;
+                continue;
+            }
+
+            ++numSubarray;
+            product *= num;
+            if (i == topSubarray) continue;
+
+            do {
+                if (product >= k) product /= nums[topSubarray++];
+                else {
+                    numSubarray += i - topSubarray;
+                    break;
+                }
+            } while (topSubarray <= i);
+        }
+
+        return numSubarray;
+    }
+}
