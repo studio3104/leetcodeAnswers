bc7fcc3edccc2f2b33e95cf1308c0cf7dee50c25
studio3104 <studio3104.com@gmail.com>
2020-10-03 03:04:40 +0900

    Answer October week1

--- java/src/test/java/com/studio3104/leetcode/explore/challenge/October/week1/SolutionTest.java
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/October/week1/SolutionTest.java
@@ -1,6 +1,6 @@
 package com.studio3104.leetcode.explore.challenge.October.week1;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,19 +8,34 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SolutionTest {
     Solution solution = new Solution();
 
     static Stream<Arguments> argumentsProvider() {
         return Stream.of(
-                Arguments.of()
+                Arguments.of(
+                        List.of(
+                                List.of(1, 2, 3),
+                                List.of(4, 5),
+                                List.of(1, 2, 3)
+                        ),
+                        4
+                ),
+                Arguments.of(
+                        List.of(
+                                List.of(-1, 1),
+                                List.of(-3, 1, 4),
+                                List.of(-2, -1, 0, 2)
+                        ),
+                        6
+                )
         );
     }
 
     @ParameterizedTest
     @MethodSource("argumentsProvider")
-    void maxDistance() {
+    void maxDistance(List<List<Integer>> arrays, int expected) {
+        int result = solution.maxDistance(arrays);
+        Assertions.assertEquals(expected, result);
     }
 }
