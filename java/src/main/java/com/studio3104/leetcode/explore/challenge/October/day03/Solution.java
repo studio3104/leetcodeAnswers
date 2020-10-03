87a7abd330606d7434bd626d6231f92785d7d231
studio3104 <studio3104.com@gmail.com>
2020-10-03 23:20:09 +0900

    Rename a variable

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day03/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day03/Solution.java
@@ -5,15 +5,15 @@ import java.util.Map;
 
 public class Solution {
     public int findPairs(int[] nums, int k) {
-        Map<Integer, Integer> numbers = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i) numbers.put(nums[i], i);
+        Map<Integer, Integer> lastIndexOf = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) lastIndexOf.put(nums[i], i);
 
         int numPairs = 0;
 
         for (int i = 0; i < nums.length; ++i) {
             int num = nums[i] + k;
-            if (!numbers.containsKey(num) || numbers.get(num) == i) continue;
-            numbers.remove(num);
+            if (!lastIndexOf.containsKey(num) || lastIndexOf.get(num) == i) continue;
+            lastIndexOf.remove(num);
             ++numPairs;
         }
 
