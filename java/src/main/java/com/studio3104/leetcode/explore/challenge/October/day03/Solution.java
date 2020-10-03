50b3224aa940b90ad2823e9c1d85604c8ab9b6a7
studio3104 <studio3104.com@gmail.com>
2020-10-03 23:16:59 +0900

    Update October 3rd

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day03/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day03/Solution.java
@@ -1,29 +1,18 @@
 package com.studio3104.leetcode.explore.challenge.October.day03;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Solution {
     public int findPairs(int[] nums, int k) {
-        Map<Integer, List<Integer>> numbers = new HashMap<>();
-
-        for (int i = 0; i < nums.length; ++i) {
-            int num = nums[i];
-            numbers.putIfAbsent(num, new ArrayList<>());
-            numbers.get(num).add(i);
-        }
+        Map<Integer, Integer> numbers = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) numbers.put(nums[i], i);
 
         int numPairs = 0;
 
         for (int i = 0; i < nums.length; ++i) {
             int num = nums[i] + k;
-            if (!numbers.containsKey(num)) continue;
-
-            List<Integer> indexes = numbers.get(num);
-            if (indexes.size() == 1 && indexes.get(0) == i) continue;
-
+            if (!numbers.containsKey(num) || numbers.get(num) == i) continue;
             numbers.remove(num);
             ++numPairs;
         }
