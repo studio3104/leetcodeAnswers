f25287811efbcf3f751f3992cf159ebbd9d9a3a9
studio3104 <studio3104.com@gmail.com>
2020-10-06 21:43:37 +0900

    Update October 6th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day06/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day06/Solution.java
@@ -3,26 +3,17 @@ package com.studio3104.leetcode.explore.challenge.October.day06;
 import com.studio3104.leetcode.structure.TreeNode;
 
 public class Solution {
+    TreeNode toAdd;
+
+    private TreeNode insertIntoBST(TreeNode tree) {
+        if (tree == null) return toAdd;
+        if (toAdd.val > tree.val) tree.right = insertIntoBST(tree.right);
+        else tree.left = insertIntoBST(tree.left);
+        return tree;
+    }
+
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if (root == null) return new TreeNode(val);
-        TreeNode current = root;
-
-        while (true) {
-            if (val > current.val) {
-                if (current.right == null) {
-                    current.right = new TreeNode(val);
-                    break;
-                }
-                current = current.right;
-            } else {
-                if (current.left == null) {
-                    current.left = new TreeNode(val);
-                    break;
-                }
-                current = current.left;
-            }
-        }
-
-        return root;
+        toAdd = new TreeNode(val);
+        return insertIntoBST(root);
     }
 }
