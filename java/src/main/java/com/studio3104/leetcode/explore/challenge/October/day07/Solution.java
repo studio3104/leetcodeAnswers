1e0559f0c2ceab31d7461d25f751f3779bb1a97e
studio3104 <studio3104.com@gmail.com>
2020-10-08 00:57:22 +0900

    Answer October 7th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day07/Solution.java
@@ -0,0 +1,29 @@
+package com.studio3104.leetcode.explore.challenge.October.day07;
+
+import com.studio3104.leetcode.structure.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k == 0) return head;
+
+        List<ListNode> nodes = new ArrayList<>();
+        ListNode current = head;
+
+        while (current != null) {
+            nodes.add(current);
+            current = current.next;
+        }
+
+        int length = nodes.size();
+        if (length <= 1 || k % length == 0) return head;
+
+        int endIndex = length - 1 - k % length;
+        nodes.get(endIndex).next = null;
+        nodes.get(length - 1).next = head;
+
+        return nodes.get(endIndex + 1);
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/October/day07/SolutionTest.java
@@ -0,0 +1,35 @@
+package com.studio3104.leetcode.explore.challenge.October.day07;
+
+import com.studio3104.leetcode.structure.ListNode;
+import com.studio3104.leetcode.structure.ListNodeConverter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, 2, new int[]{4, 5, 1, 2, 3}),
+                Arguments.of(new int[]{0, 1, 2}, 4, new int[]{2, 0, 1}),
+                Arguments.of(new int[]{1, 2}, 1, new int[]{2, 1}),
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, 10, new int[]{1, 2, 3, 4, 5})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void rotateRight(
+            @ConvertWith(ListNodeConverter.class) ListNode head,
+            int k,
+            @ConvertWith(ListNodeConverter.class) ListNode expected
+    ) {
+        ListNode result = solution.rotateRight(head, k);
+        Assertions.assertEquals(expected, result);
+    }
+}
