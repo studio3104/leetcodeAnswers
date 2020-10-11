ac6644d4b469a8e9763d0257d1e13626511c947c
studio3104 <studio3104.com@gmail.com>
2020-10-12 00:30:06 +0900

    Answer October 11th

---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/October/day11/SolutionTest.java
@@ -0,0 +1,20 @@
+package com.studio3104.leetcode.explore.challenge.October.day11;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @CsvSource({
+            "bcabc, abc",
+            "cbacdcbc, acdb",
+            "ecbacba, eacb",
+    })
+    void removeDuplicateLetters(String s, String expected) {
+        String result = solution.removeDuplicateLetters(s);
+        Assertions.assertEquals(expected, result);
+    }
+}
