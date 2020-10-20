70eed7cb25e506007f22faa34b126e002d10251a
studio3104 <studio3104.com@gmail.com>
2020-10-20 17:01:46 +0900

    Answer October 20th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day20/Solution.java
@@ -0,0 +1,47 @@
+package com.studio3104.leetcode.explore.challenge.October.day20;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+
+public class Solution {
+    Map<Integer, Node> m = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        if (m.containsKey(node.val)) return m.get(node.val);
+
+        Node clone = new Node(node.val);
+        m.put(node.val, clone);
+
+        clone.neighbors = node.neighbors
+                .stream()
+                .map(this::cloneGraph)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        return clone;
+    }
+}
