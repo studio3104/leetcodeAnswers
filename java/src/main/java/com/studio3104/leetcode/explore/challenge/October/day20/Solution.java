ca6f3e6f8c7746b1136475d41cc1e9013e4dea22
studio3104 <studio3104.com@gmail.com>
2020-10-20 18:15:51 +0900

    Update October 20th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day20/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day20/Solution.java
@@ -1,30 +1,8 @@
 package com.studio3104.leetcode.explore.challenge.October.day20;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-class Node {
-    public int val;
-    public List<Node> neighbors;
-
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<>();
-    }
-
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<>();
-    }
-
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
 
 public class Solution {
     Map<Integer, Node> m = new HashMap<>();
