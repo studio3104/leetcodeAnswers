bf4d18f7fe49c35ab9b686c313c55b0784e7789c
studio3104 <studio3104.com@gmail.com>
2020-10-21 18:17:21 +0900

    Answer October 21st

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day21/Solution.java
@@ -0,0 +1,32 @@
+package com.studio3104.leetcode.explore.challenge.October.day21;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> q = new ArrayDeque<>();
+
+        for (int asteroid : asteroids) {
+            if (asteroid > 0) {
+                q.add(asteroid);
+                continue;
+            }
+
+            boolean isDisappeared = false;
+
+            while (q.peekLast() != null && q.peekLast() > 0) {
+                int qa = q.pollLast();
+                if (qa > Math.abs(asteroid)) q.add(qa);
+                if (qa > Math.abs(asteroid) || qa == Math.abs(asteroid)) {
+                    isDisappeared = true;
+                    break;
+                }
+            }
+
+            if (!isDisappeared) q.add(asteroid);
+        }
+
+        return q.stream().mapToInt(n -> n).toArray();
+    }
+}
