579230ba720e2b4e5a138fa83ceb2f0fd09d837b
studio3104 <studio3104.com@gmail.com>
2020-10-23 01:49:19 +0900

    Answer October week 4

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/October/week4/Solution.java
@@ -0,0 +1,19 @@
+package com.studio3104.leetcode.explore.challenge.October.week4;
+
+public class Solution {
+    public int search(ArrayReader reader, int target) {
+        int head = 0, tail = (int) Math.pow(10, 4) - 1;
+
+        while (head <= tail) {
+            int pivot = head + (tail - head) / 2;
+            int val = reader.get(pivot);
+
+            if (val == target) return pivot;
+
+            if (val < target) head = pivot + 1;
+            else tail = pivot - 1;
+        }
+
+        return -1;
+    }
+}
