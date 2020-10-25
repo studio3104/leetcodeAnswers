902378e4e77bf464d50afe7f77d6d7c7c5354a70
studio3104 <studio3104.com@gmail.com>
2020-10-25 20:53:12 +0900

    Add October 25th (WA)

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day25/Solution.java
@@ -0,0 +1,44 @@
+package com.studio3104.leetcode.explore.challenge.October.day25;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+    private final List<Integer> SQUARES = IntStream
+            .range(1, (int) Math.sqrt(Math.pow(10, 5)))
+            .map(n -> (int) Math.pow(n, 2))
+            .boxed()
+            .collect(Collectors.toList());
+
+    private final Map<Integer, Boolean> memo = new HashMap<>() {{
+        put(0, false);
+        put(1, true);
+        put(2, false);
+    }};
+
+    private boolean winnerSquareGame(int n, boolean isMyTurn) {
+        if (memo.containsKey(n)) return isMyTurn == memo.get(n);
+        if (n % 3 == 0) return isMyTurn;
+
+        boolean result = false;
+
+        for (int square : SQUARES) {
+            if (n < square) break;
+
+            if (winnerSquareGame(n - square, !isMyTurn)) {
+                result = true;
+                break;
+            }
+        }
+
+        memo.put(n, result);
+        return memo.get(n);
+    }
+
+    public boolean winnerSquareGame(int n) {
+        return winnerSquareGame(n, true);
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/October/day25/SolutionTest.java
@@ -0,0 +1,26 @@
+package com.studio3104.leetcode.explore.challenge.October.day25;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, true",
+            "2, false",
+            "4, true",
+            "7, false",
+            "8, true",
+            "12, false",
+            "17, false",
+            "32, true",
+            "99857, true",
+    })
+    void winnerSquareGame(int n, boolean expected) {
+        boolean result = solution.winnerSquareGame(n);
+        Assertions.assertEquals(expected, result);
+    }
+}
