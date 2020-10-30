670880faf7affe2246ad9c81b3678efb717c39b0
studio3104 <studio3104.com@gmail.com>
2020-10-30 22:33:45 +0900

    Answer October 30th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/October/day30/Solution.java
@@ -0,0 +1,41 @@
+package com.studio3104.leetcode.explore.challenge.October.day30;
+
+import java.util.Arrays;
+
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        if (nums.length <= 1) return nums.length;
+
+        int[] longestAt = new int[nums.length];
+        int[] longestCountAt = new int[nums.length];
+        Arrays.fill(longestAt, 1);
+        Arrays.fill(longestCountAt, 1);
+
+        int maxLength = 0;
+        int maxLengthCount = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] <= nums[j]) continue;
+
+                if (longestAt[i] == longestAt[j] + 1) {
+                    longestCountAt[i] += longestCountAt[j];
+                }
+                if (longestAt[i] <= longestAt[j]) {
+                    longestCountAt[i] = longestCountAt[j];
+                    longestAt[i] = longestAt[j] + 1;
+                }
+            }
+
+            if (longestAt[i] == maxLength) {
+                maxLengthCount += longestCountAt[i];
+            }
+            if (longestAt[i] > maxLength) {
+                maxLength = longestAt[i];
+                maxLengthCount = longestCountAt[i];
+            }
+        }
+
+        return maxLengthCount;
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/October/day30/SolutionTest.java
@@ -0,0 +1,26 @@
+package com.studio3104.leetcode.explore.challenge.October.day30;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 3, 5, 4, 7}, 2),
+                Arguments.of(new int[]{2, 2, 2, 2, 2}, 5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void findNumberOfLIS(int[] nums, int expected) {
+        int actual = solution.findNumberOfLIS(nums);
+        Assertions.assertEquals(expected, actual);
+    }
+}
