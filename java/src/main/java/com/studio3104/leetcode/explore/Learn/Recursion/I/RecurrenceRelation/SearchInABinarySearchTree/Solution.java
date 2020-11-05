9eba255a3060d408afdd1e4f644f9ecec9d6e5f7
studio3104 <studio3104.com@gmail.com>
2020-11-06 00:30:40 +0900

    Update "Search in a Binary Search Tree" of "Recurrence Relation"

--- java/src/main/java/com/studio3104/leetcode/explore/Learn/Recursion/I/RecurrenceRelation/SearchInABinarySearchTree/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/Learn/Recursion/I/RecurrenceRelation/SearchInABinarySearchTree/Solution.java
@@ -4,7 +4,6 @@ import com.studio3104.leetcode.structure.TreeNode;
 
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null || root.val == val) return root;
-        return searchBST(root.val > val ? root.left : root.right, val);
+        return (root == null || root.val == val) ? root : searchBST(root.val > val ? root.left : root.right, val);
     }
 }
