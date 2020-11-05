ecfe1dd2c7be12a8c3c2cc79652a174bd8a732e1
studio3104 <studio3104.com@gmail.com>
2020-11-05 18:20:02 +0900

    Update November 5th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/November/day05/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/November/day05/Solution.java
@@ -1,15 +1,10 @@
 package com.studio3104.leetcode.explore.challenge.November.day05;
 
+import java.util.Arrays;
+
 class Solution {
     public int minCostToMoveChips(int[] position) {
-        int odd = 0;
-        int even = 0;
-
-        for (int p : position) {
-            if (p % 2 == 0) ++even;
-            else ++odd;
-        }
-
-        return Math.min(odd, even);
+        int even = (int) Arrays.stream(position).filter(p -> p % 2 == 0).count();
+        return Math.min(even, position.length - even);
     }
 }
