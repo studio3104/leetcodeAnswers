e481ee9bd23b552e8b07a5add5e7ce742318277c
studio3104 <studio3104.com@gmail.com>
2020-11-13 22:36:33 +0900

    Update November 13th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/November/day13/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/November/day13/Solution.java
@@ -1,24 +1,19 @@
 package com.studio3104.leetcode.explore.challenge.November.day13;
 
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    private void traverse(Node node, int level, Map<Integer, Node> levels) {
-        if (node == null) return;
-
-        if (levels.containsKey(level)) {
-            levels.get(level).next = node;
+    public Node connect(Node root) {
+        if (root == null || root.left == null) {
+            return root;
         }
 
-        levels.put(level, node);
+        root.left.next = root.right;
+        if (root.next != null) {
+            root.right.next = root.next.left;
+        }
 
-        traverse(node.left, level + 1, levels);
-        traverse(node.right, level + 1, levels);
-    }
+        connect(root.left);
+        connect(root.right);
 
-    public Node connect(Node root) {
-        traverse(root, 0, new HashMap<>());
         return root;
     }
 }
