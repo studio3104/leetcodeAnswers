251f16ae6cfcb9c9fcc056b27c4a99d2d0e032dc
studio3104 <studio3104.com@gmail.com>
2020-11-23 20:29:54 +0900

    Update November 23rd

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/November/day23/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/November/day23/Solution.java
@@ -3,6 +3,26 @@ package com.studio3104.leetcode.explore.challenge.November.day23;
 import com.studio3104.leetcode.structure.TreeNode;
 
 class Solution {
+    private int[] robSub(TreeNode root) {
+        if (root == null) return new int[2];
+
+        int[] left = robSub(root.left);
+        int[] right = robSub(root.right);
+        int[] result = new int[2];
+
+        result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        result[1] = root.val + left[0] + right[0];
+
+        return result;
+    }
+
+    public int rob(TreeNode root) {
+        int[] result = robSub(root);
+        return Math.max(result[0], result[1]);
+    }
+}
+
+class _Solution {
     private TreeNode constructCache(TreeNode tree, TreeNode cache) {
         if (tree != null) {
             cache.left = constructCache(tree.left, new TreeNode());
