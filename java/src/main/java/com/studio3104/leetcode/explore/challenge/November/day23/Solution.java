566a982936b37805ea9c40e87eec7dce1eff170d
studio3104 <studio3104.com@gmail.com>
2020-11-23 20:20:20 +0900

    Answer November 23rd

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/November/day23/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/November/day23/Solution.java
@@ -3,20 +3,25 @@ package com.studio3104.leetcode.explore.challenge.November.day23;
 import com.studio3104.leetcode.structure.TreeNode;
 
 class Solution {
+    private TreeNode constructCache(TreeNode tree, TreeNode cache) {
+        if (tree != null) {
+            cache.left = constructCache(tree.left, new TreeNode());
+            cache.right = constructCache(tree.right, new TreeNode());
+        }
+        return cache;
+    }
+
     private int rob(TreeNode tree, int skipCount, TreeNode cache) {
         if (tree == null) return 0;
-        if (skipCount < 0) skipCount += 2;
-        if (cache == null) cache = new TreeNode(0);
 
+        if (skipCount < 0) skipCount += 2;
         int left;
         int right;
 
         if (skipCount == 0) {
             if (cache.val > 0) return cache.val;
-
             left = Math.max(rob(tree.left, 1, cache.left), rob(tree.left, 2, cache.left));
             right = Math.max(rob(tree.right, 1, cache.right), rob(tree.right, 2, cache.right));
-
             cache.val = tree.val + left + right;
             return cache.val;
         }
@@ -29,14 +34,11 @@ class Solution {
     public int rob(TreeNode root) {
         if (root == null) return 0;
 
-        TreeNode cache = new TreeNode(0, new TreeNode(0), new TreeNode(0));
+        TreeNode cache = constructCache(root, new TreeNode());
 
         int left = Math.max(rob(root.left, 0, cache.left), rob(root.left, 1, cache.left));
         int right = Math.max(rob(root.right, 0, cache.right), rob(root.right, 1, cache.right));
 
-        return Math.max(
-                Math.max(rob(root, 0,cache), left + right),
-                Math.max(left, right)
-        );
+        return Math.max(rob(root, 0, cache), left + right);
     }
 }
