cebbc93d44b14027ac59c8f8ba38133d137be6b4
studio3104 <studio3104.com@gmail.com>
2020-11-27 03:21:10 +0900

    Update November week 4

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/November/week4/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/November/week4/Solution.java
@@ -1,14 +1,12 @@
 package com.studio3104.leetcode.explore.challenge.November.week4;
 
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 class Solution {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
         Map<Character, Integer> lastIndexOf = new HashMap<>();
-        Set<Character> elements = new HashSet<>();
 
         int head = 0;
         int longest = 0;
@@ -16,23 +14,15 @@ class Solution {
         for (int i = 0; i < s.length(); ++i) {
             char c = s.charAt(i);
             lastIndexOf.put(c, i);
-            elements.add(c);
 
-            if (elements.size() <= 2) {
+            if (lastIndexOf.size() <= 2) {
                 longest = Math.max(longest, i - head + 1);
                 continue;
             }
 
-            head = Integer.MAX_VALUE;
-            char toRemove = c;
-            for (char e : elements) {
-                if (head > lastIndexOf.get(e)) {
-                    head = lastIndexOf.get(e);
-                    toRemove = e;
-                }
-            }
-            ++head;
-            elements.remove(toRemove);
+            int indexToRemove = Collections.min(lastIndexOf.values());
+            lastIndexOf.remove(s.charAt(indexToRemove));
+            head = indexToRemove + 1;
         }
 
         return longest;
