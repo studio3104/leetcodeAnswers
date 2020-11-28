678b6814d4735b2c56157825a86c53139241e716
studio3104 <studio3104.com@gmail.com>
2020-11-28 22:38:11 +0900

    Add November 28th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/November/day28/Solution.java
@@ -0,0 +1,44 @@
+package com.studio3104.leetcode.explore.challenge.November.day28;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public void cleanDeque(int i, int k, int[] nums, Deque<Integer> deque) {
+        if (!deque.isEmpty() && deque.getFirst() == i - k) {
+            deque.removeFirst();
+        }
+
+        while (!deque.isEmpty() && nums[i] > nums[deque.getLast()]) {
+            deque.removeLast();
+        }
+    }
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length * k == 0) return new int[0];
+        if (k == 1) return nums;
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        int maxIndex = 0;
+
+        for (int i = 0; i < k; i++) {
+            cleanDeque(i, k, nums, deque);
+            deque.addLast(i);
+
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        int[] output = new int[nums.length - k + 1];
+        output[0] = nums[maxIndex];
+
+        for (int i = k; i < nums.length; i++) {
+            cleanDeque(i, k, nums, deque);
+            deque.addLast(i);
+            output[i - k + 1] = nums[deque.getFirst()];
+        }
+
+        return output;
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/November/day28/SolutionTest.java
@@ -0,0 +1,31 @@
+package com.studio3104.leetcode.explore.challenge.November.day28;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3, new int[]{3, 3, 5, 5, 6, 7}),
+                Arguments.of(new int[]{1}, 1, new int[]{1}),
+                Arguments.of(new int[]{1, -1}, 1, new int[]{1, -1}),
+                Arguments.of(new int[]{9, 11}, 2, new int[]{11}),
+                Arguments.of(new int[]{4, -2}, 2, new int[]{4}),
+                Arguments.of(new int[]{1, 3, 1, 2, 0, 5}, 3, new int[]{3, 3, 2, 5}),
+                Arguments.of(new int[]{9, 10, 9, -7, -4, -8, 2, -6}, 5, new int[]{10, 10, 9, 2})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void maxSlidingWindow(int[] nums, int k, int[] expected) {
+        int[] actual = solution.maxSlidingWindow(nums, k);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
