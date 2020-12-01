039160ce4cd2857dc0e1ae01b02d478e15b440c0
studio3104 <studio3104.com@gmail.com>
2020-12-01 20:06:30 +0900

    Answer December Week 1

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/December/week1/Solution.java
@@ -0,0 +1,24 @@
+package com.studio3104.leetcode.explore.challenge.December.week1;
+
+class Solution {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int shortest = words.length;
+        int word1Index = -1;
+        int word2Index = -1;
+
+        for (int i = 0; i < words.length; ++i) {
+            String s = words[i];
+
+            if (!s.equals(word1) && !s.equals(word2)) continue;
+
+            if (s.equals(word1)) word1Index = i;
+            if (s.equals(word2)) word2Index = i;
+
+            if (word1Index == -1 || word2Index == -1) continue;
+
+            shortest = Math.min(shortest, Math.abs(word1Index - word2Index));
+        }
+
+        return shortest;
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/December/week1/SolutionTest.java
@@ -0,0 +1,26 @@
+package com.studio3104.leetcode.explore.challenge.December.week1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of(new String[]{"practice", "makes", "perfect", "coding", "makes"}, "coding", "practice", 3),
+                Arguments.of(new String[]{"practice", "makes", "perfect", "coding", "makes"}, "makes", "coding", 1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void shortestDistance(String[] words, String word1, String word2, int expected) {
+        int actual = solution.shortestDistance(words, word1, word2);
+        Assertions.assertEquals(expected, actual);
+    }
+}
