b2c36343f43136b1259959f9afb807d5aa757fc0
studio3104 <studio3104.com@gmail.com>
2020-12-02 17:44:09 +0900

    Answer December 2nd

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day02/Solution.java
@@ -0,0 +1,24 @@
+package com.studio3104.leetcode.explore.challenge.December.day02;
+
+import com.studio3104.leetcode.structure.ListNode;
+
+// Space complexity: O(1)
+class Solution {
+    private final ListNode head;
+
+    public Solution(ListNode head) {
+        this.head = head;
+    }
+
+    public int getRandom() {
+        int value = 0;
+        ListNode current = head;
+
+        for (int i = 1; current != null; ++i) {
+            if (Math.random() < 1.0 / i) value = current.val;
+            current = current.next;
+        }
+
+        return value;
+    }
+}
