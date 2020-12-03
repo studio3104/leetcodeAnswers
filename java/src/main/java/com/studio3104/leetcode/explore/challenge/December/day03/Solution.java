7e5a0f125a59eb3c54a1b3d2867ddc5de84ab11e
studio3104 <studio3104.com@gmail.com>
2020-12-04 01:17:29 +0900

    Answer December 3rd

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day03/Solution.java
@@ -0,0 +1,24 @@
+package com.studio3104.leetcode.explore.challenge.December.day03;
+
+import com.studio3104.leetcode.structure.TreeNode;
+
+class Solution {
+    private TreeNode current;
+
+    private void rearrangeInOrder(TreeNode node) {
+        if (node == null) return;
+
+        rearrangeInOrder(node.left);
+        node.left = null;
+        current.right = node;
+        current = node;
+        rearrangeInOrder(node.right);
+    }
+
+    public TreeNode increasingBST(TreeNode root) {
+        TreeNode pseudo = new TreeNode();
+        current = pseudo;
+        rearrangeInOrder(root);
+        return pseudo.right;
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/December/day03/SolutionTest.java
@@ -0,0 +1,32 @@
+package com.studio3104.leetcode.explore.challenge.December.day03;
+
+import com.studio3104.leetcode.structure.TreeNode;
+import com.studio3104.leetcode.structure.TreeNodeConverter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of("[5,3,6,2,4,null,8,1,null,null,null,7,9]", "[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]"),
+                Arguments.of("[5,1,7]", "[1,null,5,null,7]")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void increasingBST(
+            @ConvertWith(TreeNodeConverter.class) TreeNode root,
+            @ConvertWith(TreeNodeConverter.class) TreeNode expected
+    ) {
+        TreeNode actual = solution.increasingBST(root);
+        Assertions.assertEquals(expected, actual);
+    }
+}
