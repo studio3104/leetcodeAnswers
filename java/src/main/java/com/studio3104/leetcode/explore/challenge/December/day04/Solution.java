a1731eecf1d8eeffa9c1586a9eff297059b84d42
studio3104 <studio3104.com@gmail.com>
2020-12-04 19:34:03 +0900

    Answer December 4th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day04/Solution.java
@@ -0,0 +1,18 @@
+package com.studio3104.leetcode.explore.challenge.December.day04;
+
+class Solution {
+    public int kthFactor(int n, int k) {
+        int count = 0;
+
+        for (int i = 1; i <= n; ++i) {
+            if (n % i == 0) {
+                ++count;
+            }
+            if (count == k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/December/day04/SolutionTest.java
@@ -0,0 +1,22 @@
+package com.studio3104.leetcode.explore.challenge.December.day04;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    @ParameterizedTest
+    @CsvSource({
+            "12, 3, 3",
+            "7, 2, 7",
+            "4, 4, -1",
+            "1, 1, 1",
+            "1000, 3, 4",
+    })
+    void kthFactor(int n, int k, int expected) {
+        int actual = solution.kthFactor(n, k);
+        Assertions.assertEquals(expected, actual);
+    }
+}
