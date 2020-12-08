d4165e4db881d3b945bb36e556586d70af92c444
studio3104 <studio3104.com@gmail.com>
2020-12-08 21:03:46 +0900

    Update December 8th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day08/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day08/Solution.java
@@ -1,26 +1,14 @@
 package com.studio3104.leetcode.explore.challenge.December.day08;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
         int count = 0;
-        Map<Integer, List<Integer>> indexOf = new HashMap<>();
+        int[] surpluses = new int[60];
 
-        for (int i = 0; i < time.length; ++i) {
-            int surplus = time[i] % 60;
-
-            int sub = surplus == 0 ? 0 : 60 - surplus;
-
-            if (indexOf.containsKey(sub)) {
-                count += indexOf.get(sub).size();
-            }
-
-            indexOf.putIfAbsent(surplus, new ArrayList<>());
-            indexOf.get(surplus).add(i);
+        for (int t : time) {
+            int s = t % 60;
+            count += s == 0 ? surpluses[s] : surpluses[60 - s];
+            ++surpluses[s];
         }
 
         return count;
