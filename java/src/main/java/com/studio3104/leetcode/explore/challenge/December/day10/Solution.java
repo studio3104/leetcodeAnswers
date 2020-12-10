b4e7f3bba8cc6ea86bf1d44808cc9ba5dc16cc19
studio3104 <studio3104.com@gmail.com>
2020-12-11 01:51:14 +0900

    Answer December 10th

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day10/Solution.java
@@ -0,0 +1,21 @@
+package com.studio3104.leetcode.explore.challenge.December.day10;
+
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int i = 0;
+
+        while (i + 1 < arr.length && arr[i] < arr[i + 1]) {
+            ++i;
+        }
+
+        if (i == 0 || i == arr.length - 1) {
+            return false;
+        }
+
+        while (i + 1 < arr.length && arr[i] > arr[i + 1]) {
+            ++i;
+        }
+
+        return i == arr.length - 1;
+    }
+}
