38aa3a2f1b394129cb8651fabd27a11f9e78ede6
studio3104 <studio3104.com@gmail.com>
2020-12-16 00:56:43 +0900

    Update December 15th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day15/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day15/Solution.java
@@ -2,13 +2,13 @@ package com.studio3104.leetcode.explore.challenge.December.day15;
 
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] squares = new int[nums.length];
-        int p = squares.length - 1;
         int head = 0;
-        int tail = p;
+        int tail = nums.length - 1;
 
-        while (p >= 0) {
-            squares[p--] = (int) (Math.abs(nums[head]) > Math.abs(nums[tail]) ? Math.pow(nums[head++], 2) : Math.pow(nums[tail--], 2));
+        int[] squares = new int[nums.length];
+
+        for (int p = tail; p >= 0; --p) {
+            squares[p] = (int) (Math.abs(nums[head]) > Math.abs(nums[tail]) ? Math.pow(nums[head++], 2) : Math.pow(nums[tail--], 2));
         }
 
         return squares;
