502dff4c86c2b3abaa9534f3d7a1ef7fdf4e5ce3
studio3104 <studio3104.com@gmail.com>
2020-12-17 02:41:17 +0900

    Update December 16th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day16/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day16/Solution.java
@@ -2,38 +2,20 @@ package com.studio3104.leetcode.explore.challenge.December.day16;
 
 import com.studio3104.leetcode.structure.TreeNode;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
-class Solution {
-    private void enqueueInorder(TreeNode node, Deque<TreeNode> q) {
-        if (node == null) {
-            return;
-        }
-
-        enqueueInorder(node.left, q);
-        q.add(node);
-        enqueueInorder(node.right, q);
-    }
-
-    public boolean isValidBST(TreeNode root) {
-        Deque<TreeNode> q = new ArrayDeque<>();
-        enqueueInorder(root, q);
-
-        if (q.isEmpty()) {
+public class Solution {
+    boolean isValidBST(TreeNode tree, long max, long min) {
+        if (tree == null) {
             return true;
         }
 
-        int current = q.pollFirst().val;
-
-        while (!q.isEmpty()) {
-            TreeNode node = q.pollFirst();
-            if (node.val <= current) {
-                return false;
-            }
-            current = node.val;
+        if (tree.val >= min || tree.val <= max) {
+            return false;
         }
 
-        return true;
+        return isValidBST(tree.left, max, tree.val) && isValidBST(tree.right, tree.val, min);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 }
