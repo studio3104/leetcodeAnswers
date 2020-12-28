b650ec098d76b188bb97ed466453ff676145adfc
studio3104 <studio3104.com@gmail.com>
2020-12-28 17:41:57 +0900

    Update December 28th (TLE: 31 / 73 test cases passed)

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day28/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/December/day28/Solution.java
@@ -2,26 +2,36 @@ package com.studio3104.leetcode.explore.challenge.December.day28;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 class Solution {
     public int reachNumber(int target) {
-        Deque<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{0, 0});
+        target = Math.abs(target);
+        Set<List<Integer>> processed = new HashSet<>();
+        Deque<List<Integer>> q = new ArrayDeque<>();
+        q.add(List.of(0, 0));
         int step = -1;
 
         while (!q.isEmpty()) {
-            int[] t = q.pollFirst();
-            int n = t[0];
-            int s = t[1];
+            List<Integer> t = q.pollFirst();
+            int n = Math.abs(t.get(0));
+            int s = t.get(1);
 
             if (n == target) {
                 step = s;
                 break;
             }
 
+            if (processed.contains(List.of(n, s))) {
+                continue;
+            }
+            processed.add(List.of(n, s));
+
             ++s;
-            q.add(new int[]{n + s, s});
-            q.add(new int[]{n - s, s});
+            q.add(List.of(n + s, s));
+            q.add(List.of(n - s, s));
         }
 
         return step;
