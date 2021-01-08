058ca3712be684bb0213997556c493f867f103fd
studio3104 <studio3104.com@gmail.com>
2021-01-08 18:38:09 +0900

    Update the problem of week 2 of January

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/January/week2/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/January/week2/Solution.java
@@ -1,29 +1,20 @@
 package com.studio3104.leetcode.explore.challenge.y2021.January.week2;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 class Solution {
-    private void markVisited(Node node, Set<Integer> visited) {
-        if (node == null || visited.contains(node.val)) {
-            return;
-        }
-
-        for (Node child : node.children) {
-            markVisited(child, visited);
-            visited.add(child.val);
-        }
-    }
-
     public Node findRoot(List<Node> tree) {
-        Set<Integer> visited = new HashSet<>();
+        int result = 0;
+
         for (Node node : tree) {
-            markVisited(node, visited);
+            result ^= node.val;
+            for (Node child : node.children) {
+                result ^= child.val;
+            }
         }
 
         for (Node node : tree) {
-            if (!visited.contains(node.val)) {
+            if (result == node.val) {
                 return node;
             }
         }
