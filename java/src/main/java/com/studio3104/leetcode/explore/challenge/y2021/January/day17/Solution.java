ebdcbe1216007abcc91ff1b90c67a0ac56123e3e
studio3104 <studio3104.com@gmail.com>
2021-01-17 20:08:43 +0900

    Update Jan 17th (Referring the solution)

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/January/day17/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/January/day17/Solution.java
@@ -1,22 +1,7 @@
 package com.studio3104.leetcode.explore.challenge.y2021.January.day17;
 
 class Solution {
-    private int countVowelStrings(int n, int v, int[][] c) {
-        if (n == 1) {
-            return v;
-        }
-        if (v == 1) {
-            return 1;
-        }
-
-        if (c[n][v] == 0) {
-            c[n][v] = countVowelStrings(n - 1, v, c) + countVowelStrings(n, v - 1, c);
-        }
-
-        return c[n][v];
-    }
-
     public int countVowelStrings(int n) {
-        return countVowelStrings(n, 5, new int[n + 1][6]);
+        return (n + 4) * (n + 3) * (n + 2) * (n + 1) / 24;
     }
 }
