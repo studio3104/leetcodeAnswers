a0c8a32c1b60583fbd21d7e7813c55de0b816626
studio3104 <studio3104.com@gmail.com>
2021-02-18 23:49:21 +0900

    Update Feb 18th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/February/day18/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/February/day18/Solution.java
@@ -2,14 +2,16 @@ package com.studio3104.leetcode.explore.challenge.y2021.February.day18;
 
 class Solution {
     public int numberOfArithmeticSlices(int[] A) {
-        int[] m = new int[A.length];
+        int n = 0;
         int num = 0;
 
         for (int i = 2; i < A.length; ++i) {
-            if (A[i - 1] - A[i - 2] == A[i] - A[i - 1]) {
-                m[i] = m[i - 1] + 1;
-                num += m[i];
+            if (A[i - 1] - A[i - 2] != A[i] - A[i - 1]) {
+                n = 0;
+                continue;
             }
+            n = n + 1;
+            num += n;
         }
 
         return num;
