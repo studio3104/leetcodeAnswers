b381a061cb999c3b3a4049ab3feef370cad372b9
studio3104 <studio3104.com@gmail.com>
2021-02-18 23:34:07 +0900

    Answer Feb 18th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/February/day18/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/February/day18/Solution.java
@@ -1,42 +1,17 @@
 package com.studio3104.leetcode.explore.challenge.y2021.February.day18;
 
 class Solution {
-    private int countSlices(int n) {
-        int sum = 0;
-        for (int i = n; i >= 3; --i) {
-            sum += n - i + 1;
-        }
-        return sum;
-    }
-
     public int numberOfArithmeticSlices(int[] A) {
-        Integer prev = null;
-        Integer diff = null;
-        int consecutive = 0;
+        int[] m = new int[A.length];
         int num = 0;
 
-        for (int n : A) {
-            if (prev == null) {
-                prev = n;
-                continue;
+        for (int i = 2; i < A.length; ++i) {
+            if (A[i - 1] - A[i - 2] == A[i] - A[i - 1]) {
+                m[i] = m[i - 1] + 1;
+                num += m[i];
             }
-            if (diff == null) {
-                diff = Math.abs(n - prev);
-                prev = n;
-                consecutive = 2;
-                continue;
-            }
-            if (diff != Math.abs(n - prev)) {
-                num += countSlices(consecutive);
-                prev = n;
-                diff = null;
-                consecutive = 1;
-                continue;
-            }
-            prev = n;
-            ++consecutive;
         }
 
-        return num + countSlices(consecutive);
+        return num;
     }
 }
