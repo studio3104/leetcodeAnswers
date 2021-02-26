7a8b6602dee7de0dfc80711ed06499781293414c
studio3104 <studio3104.com@gmail.com>
2021-02-27 01:23:57 +0900

    Lombok?????????

--- java/src/test/java/com/studio3104/leetcode/explore/challenge/y2021/February/day22/SolutionTest.java
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/y2021/February/day22/SolutionTest.java
@@ -2,27 +2,38 @@ package com.studio3104.leetcode.explore.challenge.y2021.February.day22;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 class SolutionTest {
-    static Stream<Arguments> argumentsProvider() {
+    @lombok.AllArgsConstructor
+    static class TestParameter {
+        final String s;
+        final List<String> d;
+        final String expected;
+
+//        TestParameter(String s, List<String> d, String expected) {
+//            this.s = s;
+//            this.d = d;
+//            this.expected = expected;
+//        }
+    }
+
+    static Stream<TestParameter> argumentsProvider() {
         return Stream.of(
-                Arguments.of("abpcplea", List.of("ale", "apple", "monkey", "plea"), "apple"),
-                Arguments.of("abpcplea", List.of("a", "b", "c"), "a"),
-                Arguments.of("aaa", List.of("aaa", "aa", "a"), "aaa")
+                new TestParameter("abpcplea", List.of("ale", "apple", "monkey", "plea"), "apple"),
+                new TestParameter("abpcplea", List.of("a", "b", "c"), "a"),
+                new TestParameter("aaa", List.of("aaa", "aa", "a"), "aaa")
         );
     }
 
     @ParameterizedTest
     @MethodSource("argumentsProvider")
-    void findLongestWord(String s, List<String> d, String expected) {
+    void findLongestWord(TestParameter p) {
         Solution solution = new Solution();
-        String actual = solution.findLongestWord(s, new ArrayList<>(d));
-        Assertions.assertEquals(expected, actual);
+        String actual = solution.findLongestWord(p.s, p.d);
+        Assertions.assertEquals(p.expected, actual);
     }
 }
