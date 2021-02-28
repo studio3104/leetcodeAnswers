cfe5fe8cfdea87ba9941e2e7252ef49a247309b0
studio3104 <studio3104.com@gmail.com>
2021-03-01 00:30:59 +0900

    Answer Feb 28th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/February/day28/FreqStack.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/February/day28/FreqStack.java
@@ -1,86 +1,39 @@
 package com.studio3104.leetcode.explore.challenge.y2021.February.day28;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Set;
+import java.util.Stack;
 
 class FreqStack {
-    private final LinkedList<Integer> stack;
-    private final Map<Integer, Integer> counter;
-    private final Set<Integer> mostFrequentElements;
-    private int mostFrequentCount = 0;
+    Map<Integer, Integer> frequencyOf;
+    Map<Integer, Stack<Integer>> stackPerFrequencyOf;
+    int maxFrequency;
 
     public FreqStack() {
-        stack = new LinkedList<>();
-        counter = new HashMap<>();
-        mostFrequentElements = new HashSet<>();
+        frequencyOf = new HashMap<>();
+        stackPerFrequencyOf = new HashMap<>();
+        maxFrequency = 0;
     }
 
     public void push(int x) {
-        stack.push(x);
-        counter.put(x, counter.getOrDefault(x, 0) + 1);
+        int f = frequencyOf.getOrDefault(x, 0) + 1;
+        frequencyOf.put(x, f);
 
-        int count = counter.get(x);
-
-        if (count < mostFrequentCount) {
-            return;
+        if (f > maxFrequency) {
+            maxFrequency = f;
         }
 
-        if (count > mostFrequentCount) {
-            mostFrequentCount = count;
-            mostFrequentElements.clear();
-        }
-        mostFrequentElements.add(x);
+        stackPerFrequencyOf.computeIfAbsent(f, z -> new Stack<>()).push(x);
     }
 
     public int pop() {
-        if (stack.isEmpty()) {
-            return -1;
+        int x = stackPerFrequencyOf.get(maxFrequency).pop();
+        frequencyOf.put(x, frequencyOf.get(x) - 1);
+
+        if (stackPerFrequencyOf.get(maxFrequency).isEmpty()) {
+            --maxFrequency;
         }
 
-        if (mostFrequentElements.isEmpty()) {
-            updateMostFrequentElements();
-        }
-
-        int i = 0;
-        for (; i < stack.size(); ++i) {
-            if (mostFrequentElements.contains(stack.get(i))) {
-                break;
-            }
-        }
-
-        int n = stack.remove(i);
-        mostFrequentElements.remove(n);
-
-        if (counter.get(n) == 1) {
-            counter.remove(n);
-        } else {
-            counter.put(n, counter.get(n) - 1);
-        }
-
-        return n;
-    }
-
-    private void updateMostFrequentElements() {
-        int maxCount = 0;
-
-        for (Map.Entry<Integer, Integer> e : counter.entrySet()) {
-            int n = e.getKey();
-            int c = e.getValue();
-
-            if (c <= 0 || c < maxCount) {
-                continue;
-            }
-
-            if (c > maxCount) {
-                maxCount = c;
-                mostFrequentElements.clear();
-            }
-            mostFrequentElements.add(n);
-        }
-
-        mostFrequentCount = maxCount;
+        return x;
     }
 }
