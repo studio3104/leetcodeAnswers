d7778d5aaa176bb9ad2eca2c2bec61f803b3bac1
studio3104 <studio3104.com@gmail.com>
2021-03-25 00:23:45 +0900

    Answer Mar 24th (WA: 20 / 67 test cases passed)

---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/y2021/March/day24/Solution.java
@@ -0,0 +1,46 @@
+package com.studio3104.leetcode.explore.challenge.y2021.March.day24;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] advantageCount(int[] A, int[] B) {
+        Map<Integer, Queue<Integer>> bIndexesOf = new HashMap<>();
+        for (int i = 0; i < B.length; ++i) {
+            int n = B[i];
+            bIndexesOf.putIfAbsent(n, new ArrayDeque<>());
+            bIndexesOf.get(n).add(i);
+        }
+
+        PriorityQueue<Integer> pqA = Arrays.stream(A).boxed().collect(Collectors.toCollection(PriorityQueue::new));
+        PriorityQueue<Integer> pqB = Arrays.stream(B).boxed().collect(Collectors.toCollection(PriorityQueue::new));
+        Queue<Integer> remains = new ArrayDeque<>();
+        int[] result = new int[A.length];
+        Arrays.fill(result, -1);
+
+        while (!pqA.isEmpty()) {
+            int a = pqA.poll();
+            int b = pqB.peek();
+            if (b > a) {
+                remains.add(a);
+                continue;
+            }
+            int i = bIndexesOf.get(pqB.poll()).poll();
+            result[i] = a;
+        }
+
+        for (int i = 0; i < result.length; ++i) {
+            int n = result[i];
+            if (n == -1) {
+                result[i] = remains.poll();
+            }
+        }
+
+        return result;
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/y2021/March/day24/SolutionTest.java
@@ -0,0 +1,25 @@
+package com.studio3104.leetcode.explore.challenge.y2021.March.day24;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 7, 11, 15}, new int[]{1, 10, 4, 11}, new int[]{2, 11, 7, 15}),
+                Arguments.of(new int[]{12, 24, 8, 32}, new int[]{13, 25, 32, 11}, new int[]{24, 32, 8, 12})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void advantageCount(int[] A, int[] B, int[] expected) {
+        Solution solution = new Solution();
+        int[] actual = solution.advantageCount(A, B);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
