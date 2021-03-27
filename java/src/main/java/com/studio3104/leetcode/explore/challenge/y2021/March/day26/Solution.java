61cb6e5a12562dce01826a306de8727fd2ea3517
studio3104 <studio3104.com@gmail.com>
2021-03-27 15:37:21 +0900

    Answer Mar 26th

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/March/day26/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/March/day26/Solution.java
@@ -1,38 +1,38 @@
 package com.studio3104.leetcode.explore.challenge.y2021.March.day26;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 class Solution {
-    private boolean isSubset(Map<Character, Integer> countOf, List<Map<Character, Integer>> bCounters) {
-        for (Map<Character, Integer> counter : bCounters) {
-            for (char c : counter.keySet()) {
-                if (!countOf.containsKey(c) || countOf.get(c) < counter.get(c)) {
-                    return false;
-                }
+    private int[] countChar(String S) {
+        int[] counter = new int[26];
+        for (char c: S.toCharArray()) {
+            ++counter[c - 'a'];
+        }
+        return counter;
+    }
+
+    private boolean isSubset(int[] aCount, int[] bMax) {
+        for (int i = 0; i < 26; ++i) {
+            if (aCount[i] < bMax[i]) {
+                return false;
             }
         }
         return true;
     }
 
-    private Map<Character, Integer> countChar(String word) {
-        Map<Character, Integer> counter = new HashMap<>();
-        for (char c : word.toCharArray()) {
-            counter.put(c, counter.getOrDefault(c, 0) + 1);
-        }
-        return counter;
-    }
-
     public List<String> wordSubsets(String[] A, String[] B) {
-        List<Map<Character, Integer>> bCounters = Arrays.stream(B)
-                .map(this::countChar)
-                .collect(Collectors.toList());
+        int[] bMax = countChar("");
+        for (String b: B) {
+            int[] bCount = countChar(b);
+            for (int i = 0; i < 26; ++i) {
+                bMax[i] = Math.max(bMax[i], bCount[i]);
+            }
+        }
 
         return Arrays.stream(A)
-                .filter(s -> isSubset(countChar(s), bCounters))
+                .filter(s -> isSubset(countChar(s), bMax))
                 .collect(Collectors.toList());
     }
 }
