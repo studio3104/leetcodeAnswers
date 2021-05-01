6d9404191cf99b74bb39d33459650e1f70f73674
studio3104 <studio3104.com@gmail.com>
2021-05-02 01:27:37 +0900

    Answer May week1 (WA: 27 / 44 test cases passed.)

---
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/May/week1/Solution.java
@@ -0,0 +1,44 @@
+package com.studio3104.leetcode.explore.challenge.y2021.May.week1;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+    private void traverseAndRemove(int node, Map<Integer, Set<Integer>> graph) {
+        if (!graph.containsKey(node)) {
+            return;
+        }
+
+        Set<Integer> nodes = graph.get(node);
+        graph.remove(node);
+
+        for (int n : nodes) {
+            traverseAndRemove(n, graph);
+        }
+    }
+
+    public int countComponents(int n, int[][] edges) {
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int[] edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+            graph.putIfAbsent(a, new HashSet<>());
+            graph.putIfAbsent(b, new HashSet<>());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        int counter = 0;
+        for (int i = 0; i < n; ++i) {
+            if (!graph.containsKey(i)) {
+                continue;
+            }
+            traverseAndRemove(i, graph);
+            ++counter;
+        }
+
+        return counter;
+    }
+}
---
+++ java/src/test/java/com/studio3104/leetcode/explore/challenge/y2021/May/week1/SolutionTest.java
@@ -0,0 +1,25 @@
+package com.studio3104.leetcode.explore.challenge.y2021.May.week1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+    static Stream<Arguments> argumentsProvider() {
+        return Stream.of(
+                Arguments.of(5, new int[][]{{0, 1}, {1, 2}, {3, 4}}, 2),
+                Arguments.of(5, new int[][]{{0, 1}, {1, 2}, {2, 3}, {3, 4}}, 1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    void countComponents(int n, int[][] edges, int expected) {
+        Solution solution = new Solution();
+        int actual = solution.countComponents(n, edges);
+        Assertions.assertEquals(expected, actual);
+    }
+}
