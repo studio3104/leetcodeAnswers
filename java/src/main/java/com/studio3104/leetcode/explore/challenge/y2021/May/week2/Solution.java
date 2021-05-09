97770d3a403b21d24215b07bd8c0f1fcc0feb603
studio3104 <studio3104.com@gmail.com>
2021-05-09 20:16:51 +0900

    Reduce space usage

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/May/week2/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/May/week2/Solution.java
@@ -1,25 +1,28 @@
 package com.studio3104.leetcode.explore.challenge.y2021.May.week2;
 
 import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.stream.Collectors;
 
 class Solution {
     public int maxBoxesInWarehouse(int[] boxes, int[] warehouse) {
         int limitSoFar = warehouse[0];
-        int[] capacities = new int[warehouse.length];
         for (int i = 0; i < warehouse.length; ++i) {
             limitSoFar = Math.min(limitSoFar, warehouse[i]);
-            capacities[i] = limitSoFar;
+            warehouse[i] = limitSoFar;
         }
 
-        PriorityQueue<Integer> boxQueue = Arrays.stream(boxes).boxed().collect(Collectors.toCollection(PriorityQueue::new));
+        Arrays.sort(boxes);
+        int boxIndex = 0;
         int numBoxes = 0;
-        for (int i = capacities.length - 1; i >= 0 && !boxQueue.isEmpty(); --i) {
-            if (boxQueue.peek() > capacities[i]) {
+        for (int i = warehouse.length - 1; i >= 0; --i) {
+            if (boxIndex >= boxes.length) {
+                break;
+            }
+
+            if (boxes[boxIndex] > warehouse[i]) {
                 continue;
             }
-            boxQueue.poll();
+
+            ++boxIndex;
             ++numBoxes;
         }
 
