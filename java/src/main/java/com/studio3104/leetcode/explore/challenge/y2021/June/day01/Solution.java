8a24c588f164189b0a9ebae5bb917717893a8d10
studio3104 <studio3104.com@gmail.com>
2021-06-01 22:19:05 +0900

    Update June 1st (46ms -> 5ms)

--- java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/June/day01/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/explore/challenge/y2021/June/day01/Solution.java
@@ -1,59 +1,33 @@
 package com.studio3104.leetcode.explore.challenge.y2021.June.day01;
 
-import java.util.ArrayDeque;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Queue;
-import java.util.Set;
-
 class Solution {
-    private int BFS(int n, int m, int[][] grid, Queue<List<Integer>> q, Set<List<Integer>> memo) {
-        q.clear();
-        q.add(List.of(n, m));
-        int area = 0;
-
-        while (!q.isEmpty()) {
-            List<Integer> p = q.poll();
-            if (memo.contains(p)) {
-                continue;
-            }
-
-            int i = p.get(0);
-            int j = p.get(1);
-
-            if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] != 1) {
-                continue;
-            }
-
-            ++area;
-            memo.add(p);
-
-            q.add(List.of(i + 1, j));
-            q.add(List.of(i - 1, j));
-            q.add(List.of(i, j + 1));
-            q.add(List.of(i, j - 1));
-        }
-
-        return area;
-    }
-
     public int maxAreaOfIsland(int[][] grid) {
-        int area = 0;
-
-        Set<List<Integer>> memo = new HashSet<>();
-        Queue<List<Integer>> q = new ArrayDeque<>();
+        int maxArea = 0;
 
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[i].length; ++j) {
-                int n = grid[i][j];
-
-                if (n != 1 || memo.contains(List.of(i, j))) {
-                    continue;
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, DFS(grid, i, j));
                 }
-
-                area = Math.max(area, BFS(i, j, grid, q, memo));
             }
         }
+
+        return maxArea;
+    }
+
+    public int DFS(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) {
+            return 0;
+        }
+
+        grid[i][j] = 0;
+
+        int area = 1;
+
+        area += DFS(grid, i + 1, j);
+        area += DFS(grid, i - 1, j);
+        area += DFS(grid, i, j + 1);
+        area += DFS(grid, i, j - 1);
 
         return area;
     }
