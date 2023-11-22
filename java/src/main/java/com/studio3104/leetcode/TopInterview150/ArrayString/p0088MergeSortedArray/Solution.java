f3bec74f0be17cadbbe864860af3f864270da966
Satoshi SUZUKI <studio3104.com@gmail.com>
2023-11-22 16:58:25 +0900

    Update P0088 Merge Sorted Array

--- java/src/main/java/com/studio3104/leetcode/TopInterview150/ArrayString/p0088MergeSortedArray/Solution.java
+++ java/src/main/java/com/studio3104/leetcode/TopInterview150/ArrayString/p0088MergeSortedArray/Solution.java
@@ -2,30 +2,15 @@ package com.studio3104.leetcode.TopInterview150.ArrayString.p0088MergeSortedArra
 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = 0;
-        int p2 = 0;
-        int[] result = new int[m + n];
+        int p1 = m - 1;
+        int p2 = n - 1;
 
-        for (int p = 0; p < m + n; ++p) {
-            int n1 = Integer.MAX_VALUE;
-            int n2 = Integer.MAX_VALUE;
-
-            if (p1 < m) {
-                n1 = nums1[p1];
-            }
-            if (p2 < n) {
-                n2 = nums2[p2];
-            }
-
-            if (n1 < n2) {
-                result[p] = n1;
-                ++p1;
+        for (int p = m + n - 1; p >= 0 && p2 >= 0; --p) {
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1--];
             } else {
-                result[p] = n2;
-                ++p2;
+                nums1[p] = nums2[p2--];
             }
         }
-
-        if (m + n >= 0) System.arraycopy(result, 0, nums1, 0, m + n);
     }
 }
